1dbd6457a22a26dc709bbd9de6be060a74badbbd
mrogowska <m.rogowska@kainos.com>
2021-11-04 15:34:59 +0100

    NIAD-1853: Add GP2GP Translator skeleton

---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/gpc/Gp2gpTranslatorApplication.java
@@ -0,0 +1,11 @@
+package uk.nhs.adaptors.gpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gp2gpTranslatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Gp2gpTranslatorApplication.class, args);
+    }
+}
