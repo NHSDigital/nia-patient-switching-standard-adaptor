44897e06f7ce220c11d574d77414c35cd1d69e24
mrogowska <m.rogowska@kainos.com>
2021-12-13 14:32:19 +0100

    Restore docker network, use db container name

--- docker/clear-docker.sh
+++ docker/clear-docker.sh
@@ -2,3 +2,4 @@
 set -ex
 
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && docker volume rm $(docker volume ls -qf dangling=true)
+docker network rm pss-network
