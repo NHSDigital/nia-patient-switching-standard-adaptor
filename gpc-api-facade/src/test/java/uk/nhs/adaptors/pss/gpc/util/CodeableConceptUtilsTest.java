e5901a117f19402d5a654f0460b2fcd22efa9137
mrogowska <m.rogowska@kainos.com>
2022-01-13 14:41:44 +0100

    NIAD-1887: Add unit tests for utils classes

---
+++ gpc-api-facade/src/test/java/uk/nhs/adaptors/pss/gpc/util/CodeableConceptUtilsTest.java
@@ -0,0 +1,33 @@
+package uk.nhs.adaptors.pss.gpc.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.junit.jupiter.api.Test;
+
+public class CodeableConceptUtilsTest {
+    private static final String CODE = "RESOURCE_NOT_FOUND";
+    private static final String ISSUE_SYSTEM = "Spine-ErrorOrWarningCode-1";
+    private static final String DISPLAY = "Resource not found";
+    private static final String TEXT = "Resource got lost";
+
+    @Test
+    public void testCreateCodeableConcept() {
+        CodeableConcept result = CodeableConceptUtils.createCodeableConcept(CODE, ISSUE_SYSTEM, DISPLAY, TEXT);
+
+        assertThat(result.getCodingFirstRep().getCode()).isEqualTo(CODE);
+        assertThat(result.getCodingFirstRep().getSystem()).isEqualTo(ISSUE_SYSTEM);
+        assertThat(result.getCodingFirstRep().getDisplay()).isEqualTo(DISPLAY);
+        assertThat(result.getText()).isEqualTo(TEXT);
+    }
+
+    @Test
+    public void testCreateCodeableConceptWithNullText() {
+        CodeableConcept result = CodeableConceptUtils.createCodeableConcept(CODE, ISSUE_SYSTEM, DISPLAY, null);
+
+        assertThat(result.getCodingFirstRep().getCode()).isEqualTo(CODE);
+        assertThat(result.getCodingFirstRep().getSystem()).isEqualTo(ISSUE_SYSTEM);
+        assertThat(result.getCodingFirstRep().getDisplay()).isEqualTo(DISPLAY);
+        assertThat(result.getText()).isNull();
+    }
+}
