dc508c36001a31194b0aebe1e865e2f56b6ba9b2
bchotkowski <bartosz.chotkowski@kainos.com>
2022-01-13 15:39:10 +0100

    Added logic to the JMS message handler

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/task/QueueMessageHandler.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/task/QueueMessageHandler.java
@@ -1,25 +1,41 @@
 package uk.nhs.adaptors.pss.translator.task;
+import java.util.UUID;
 
 import javax.jms.Message;
 
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Parameters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import uk.nhs.adaptors.pss.translator.service.EhrExtractRequestService;
+import uk.nhs.adaptors.pss.translator.utils.FhirParser;
 
 @Slf4j
 @Component
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class QueueMessageHandler {
+
+    private final FhirParser fhirParser;
+    private final EhrExtractRequestService ehrExtractRequestService;
 
     @SneakyThrows
     public boolean handle(Message message) {
         LOGGER.info("Handling message with message_id=[{}]", message.getJMSMessageID());
 
-        //TODO: Add tasks here in the future?
+        var parsed = fhirParser.parseResource(message.getBody(String.class), Parameters.class);
+        String conversationId = UUID.randomUUID().toString();
+        String nhsNumber = ((Identifier) parsed.getParameterFirstRep().getValue()).getValue();
+        String fromODSCode = "ODS_CODE_HERE"; //TODO: Figure later
 
-        //TODO: Invoke a mapper from FHIR to HL7 [Map a transfer request to EhrExtractRequest]
-        //       Make sure everything is mapped correctly
-        //       Send stuff to the MHS
+        String ehrExtractRequest = ehrExtractRequestService.buildEhrExtractRequest(
+            conversationId,
+            nhsNumber,
+            fromODSCode
+        );
 
         return true;
     }
