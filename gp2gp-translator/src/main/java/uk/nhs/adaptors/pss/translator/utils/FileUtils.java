0a44052e21db6274cef1fdd56aa3ce9cbbbf426b
bchotkowski <bartosz.chotkowski@kainos.com>
2022-01-13 15:20:41 +0100

    Added FileUtils to properly read EhrExtractRequest template file

---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/utils/FileUtils.java
@@ -0,0 +1,19 @@
+package uk.nhs.adaptors.pss.translator.utils;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+
+public class FileUtils {
+
+    public static String readFile(String path) throws IOException {
+        try (InputStream is = FileUtils.class.getResourceAsStream(path)) {
+            if (is == null) { throw new FileNotFoundException(path); }
+            return IOUtils.toString(is, StandardCharsets.UTF_8);
+        }
+    }
+
+}
