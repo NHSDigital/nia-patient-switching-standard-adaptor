4e86d7098050f6763c06132280eb8c2b6388b212
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2022-01-18 16:43:55 +0000

    fixed assertion

--- gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/Generator/BundleGeneratorTest.java
+++ gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/Generator/BundleGeneratorTest.java
@@ -1,21 +1,24 @@
 package uk.nhs.adaptors.pss.translator.Generator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import java.util.stream.Stream;
 
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import uk.nhs.adaptors.pss.translator.generator.BundleGenerator;
 import uk.nhs.adaptors.pss.translator.utils.ResourceTestFileUtils;
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({SpringExtension.class})
+@DirtiesContext
 public class BundleGeneratorTest {
 
     private static final BundleGenerator BUNDLE = new BundleGenerator();
@@ -32,7 +35,7 @@ public class BundleGeneratorTest {
     public void When_GeneratingBundleResource_Expect_BundleResourceJson(String outputBundle)
         throws IOException {
         CharSequence expectedOutputJson = ResourceTestFileUtils.getFileContent(outputBundle);
-        AssertionsForClassTypes.assertThat(BUNDLE).isEqualTo(expectedOutputJson);
+        assertThat(BUNDLE).isEqualTo(expectedOutputJson);
 
     }
 }
