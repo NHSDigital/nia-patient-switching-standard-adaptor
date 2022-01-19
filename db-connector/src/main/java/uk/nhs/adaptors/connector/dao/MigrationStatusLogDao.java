601f76a4468b3f700c0a26210f65952345c5bc23
bchotkowski <bartosz.chotkowski@kainos.com>
2022-01-19 11:11:48 +0100

    Rollback to the migration original state

---
+++ db-connector/src/main/java/uk/nhs/adaptors/connector/dao/MigrationStatusLogDao.java
@@ -0,0 +1,22 @@
+package uk.nhs.adaptors.connector.dao;
+
+import java.time.OffsetDateTime;
+
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.locator.UseClasspathSqlLocator;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import uk.nhs.adaptors.connector.model.MigrationStatusLog;
+
+public interface MigrationStatusLogDao {
+
+    @SqlUpdate("insert_migration_status_log")
+    @UseClasspathSqlLocator
+    void addMigrationStatusLog(@Bind("nhsNumber") String patientNhsNumber, @Bind("status") String status, @Bind("date") OffsetDateTime date);
+
+    @SqlQuery("select_migration_status_log")
+    @UseClasspathSqlLocator
+    MigrationStatusLog getMigrationStatusLog(@Bind("nhsNumber") String patientNhsNumber);
+
+}
