7f681ac4d1317167959e7f54d91193f315e1b7c7
Barry Bell <gavin.bell@kainos.com>
2022-01-25 14:54:34 +0000

    Address review comments

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/QuantityMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/QuantityMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.Quantity;
+import org.hl7.v3.PQInc;
 import org.hl7.v3.PQR;
 import org.hl7.v3.Value;
 
@@ -17,27 +18,12 @@ public class QuantityMapper {
 
         if (TYPE_IVL_PQ.equals(value.getType())) {
             if (value.getHigh() != null) {
-                if (value.getHigh().isInclusive()) {
-                    quantity.setComparator(Quantity.QuantityComparator.LESS_OR_EQUAL);
-                } else {
-                    quantity.setComparator(Quantity.QuantityComparator.LESS_THAN);
-                }
-
-                quantity.setValue(Long.parseLong(value.getHigh().getValue()));
-                setUnit(quantity, value.getHigh().getUnit(), value.getHigh().getTranslation());
+                setHighComparator(quantity, value.getHigh());
             } else if (value.getLow() != null) {
-                if (value.getLow().isInclusive()) {
-                    quantity.setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
-                } else {
-                    quantity.setComparator(Quantity.QuantityComparator.GREATER_THAN);
-                }
-
-                quantity.setValue(Long.parseLong(value.getLow().getValue()));
-                setUnit(quantity, value.getLow().getUnit(), value.getLow().getTranslation());
+                setLowComparator(quantity, value.getLow());
             }
         } else {
-            quantity.setValue(Long.parseLong(value.getValue()));
-            setUnit(quantity, value.getUnit(), value.getTranslation());
+            setQuantityValueAndUnit(quantity, value.getValue(), value.getUnit(), value.getTranslation());
         }
 
         return quantity;
@@ -45,8 +31,7 @@ public class QuantityMapper {
 
     private void setUnit(Quantity quantity, String unit, List<PQR> translation) {
         if (StringUtils.isNotBlank(unit)) {
-            if (translation != null
-                && translation.size() > 0) {
+            if (translation != null && !translation.isEmpty()) {
                 quantity.setUnit(translation.get(0).getOriginalText());
             } else {
                 quantity.setUnit(unit);
@@ -54,5 +39,30 @@ public class QuantityMapper {
                 quantity.setCode(unit);
             }
         }
+    }
+
+    private void setHighComparator(Quantity quantity, PQInc high) {
+        if (high.isInclusive()) {
+            quantity.setComparator(Quantity.QuantityComparator.LESS_OR_EQUAL);
+        } else {
+            quantity.setComparator(Quantity.QuantityComparator.LESS_THAN);
+        }
+
+        setQuantityValueAndUnit(quantity, high.getValue(), high.getUnit(), high.getTranslation());
+    }
+
+    private void setLowComparator(Quantity quantity, PQInc low) {
+        if (low.isInclusive()) {
+            quantity.setComparator(Quantity.QuantityComparator.GREATER_OR_EQUAL);
+        } else {
+            quantity.setComparator(Quantity.QuantityComparator.GREATER_THAN);
+        }
+
+        setQuantityValueAndUnit(quantity, low.getValue(), low.getUnit(), low.getTranslation());
+    }
+
+    private void setQuantityValueAndUnit(Quantity quantity, String value, String unit, List<PQR> translation) {
+        setUnit(quantity, unit, translation);
+        quantity.setValue(Long.parseLong(value));
     }
 }
