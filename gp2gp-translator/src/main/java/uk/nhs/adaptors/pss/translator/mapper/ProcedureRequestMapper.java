a111072e095b7ac82c38c1c67ebce8514c55bd56
Rory Sproule <52039706+rorysproule1@users.noreply.github.com>
2022-01-26 16:44:05 +0000

    Beginning to map attributes (id, identifier, meta, intent, status)

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/ProcedureRequestMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/ProcedureRequestMapper.java
@@ -1,17 +1,61 @@
 package uk.nhs.adaptors.pss.translator.mapper;
 
-import org.hl7.fhir.dstu3.model.*;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.Annotation;
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.ProcedureRequest;
+import org.hl7.fhir.dstu3.model.ProcedureRequest.ProcedureRequestStatus;
+import org.hl7.fhir.dstu3.model.ProcedureRequest.ProcedureRequestIntent;
+import org.hl7.fhir.dstu3.model.UriType;
+import org.hl7.v3.CV;
 import org.hl7.v3.RCMRMT030101UK04PlanStatement;
 
 public class ProcedureRequestMapper {
+    private static final String META_PROFILE = "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-ProcedureRequest-1";
+    private static final String IDENTIFIER_SYSTEM = "https://PSSAdaptor/";
+    private static final String PRIORITY_PREFIX = "Priority: ";
 
     public ProcedureRequest mapToProcedureRequest(RCMRMT030101UK04PlanStatement planStatement) {
+        var id = planStatement.getId().getRoot();
+        var identifier = getIdentifier(id);
+        var note = new Annotation();
+        if (planStatement.getPriorityCode() != null) {
+            note = getPriority(planStatement.getPriorityCode());
+        }
 
-        return createProcedureRequest();
+        return createProcedureRequest(id, identifier);
     }
 
-    private ProcedureRequest createProcedureRequest() {
+    private Annotation getPriority(CV priorityCode) {
+        var note = new Annotation();
+        var originalText = priorityCode.getOriginalText();
+        var displayName = priorityCode.getDisplayName();
+
+        if (StringUtils.isNotEmpty(originalText)) {
+            return note.setText(PRIORITY_PREFIX + originalText);
+        } else if (StringUtils.isNotEmpty(displayName)) {
+            return note.setText(PRIORITY_PREFIX + displayName);
+        }
+
+        return null;
+    }
+
+    private Identifier getIdentifier(String id) {
+        Identifier identifier = new Identifier()
+                .setSystem(IDENTIFIER_SYSTEM) // TODO: concatenate source practice org id to URL
+                .setValue(id);
+        return identifier;
+    }
+
+    private ProcedureRequest createProcedureRequest(String id, Identifier identifier) {
         var procedureRequest = new ProcedureRequest();
+
+        procedureRequest
+                .setStatus(ProcedureRequestStatus.UNKNOWN)
+                .setIntent(ProcedureRequestIntent.ORDER)
+                .setId(id);
+        procedureRequest.getMeta().getProfile().add(new UriType(META_PROFILE));
+        procedureRequest.getIdentifier().add(identifier);
 
         return procedureRequest;
     }
