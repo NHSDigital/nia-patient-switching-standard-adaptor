1006f0452d0991a2c84deafb66c42475d053e9da
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2022-01-26 12:48:52 +0000

    NIAD-1938: Generate bundle resource
    Generate bundle resource

---
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/generator/BundleGeneratorTest.java
@@ -0,0 +1,43 @@
+package uk.nhs.adaptors.pss.translator.generator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import uk.nhs.adaptors.pss.translator.service.FhirIdGeneratorService;
+
+@ExtendWith(MockitoExtension.class)
+public class BundleGeneratorTest {
+
+    @Mock
+    private FhirIdGeneratorService fhirIdGeneratorService;
+
+    @InjectMocks
+    private BundleGenerator bundleGenerator;
+
+    private static final String META_PROFILE = "https://fhir.nhs.uk/STU3/StructureDefinition/GPConnect-StructuredRecord-Bundle-1";
+    private static final String BUNDLE_ID = "219b7cb8-da9e-447a-b279-82be7a3299da";
+
+    @BeforeEach
+    public void setUp() {
+        when(fhirIdGeneratorService.generateUuid()).thenReturn(BUNDLE_ID);
+    }
+
+    @Test
+    public void generateBundleWithCorrectDetails() {
+        var bundle = bundleGenerator.generateBundle();
+
+        assertThat(bundle.getResourceType().toString()).isEqualTo(ResourceType.Bundle.name());
+        assertThat(bundle.getId()).isEqualTo(BUNDLE_ID);
+        assertThat(bundle.getType()).isEqualTo(Bundle.BundleType.COLLECTION);
+        assertThat(bundle.getMeta().getProfile().get(0).getValue()).isEqualTo(META_PROFILE);
+    }
+}
