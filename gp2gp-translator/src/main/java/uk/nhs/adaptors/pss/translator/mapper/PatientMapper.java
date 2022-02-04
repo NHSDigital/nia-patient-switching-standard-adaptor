da40a0dbdacfd54c46b660aa5b863ffccdf74823
bchotkowski <bartosz.chotkowski@kainos.com>
2022-02-04 11:18:46 +0100

    Removed unnecessary ID from test XML

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/PatientMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/PatientMapper.java
@@ -1,35 +1,51 @@
 package uk.nhs.adaptors.pss.translator.mapper;
 
 import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Meta;
+import org.hl7.fhir.dstu3.model.Organization;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.v3.RCMRMT030101UK04Patient;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PatientMapper {
 
-    private static final String IDENTIFIER_SYSTEM = "https://PSSAdaptor/";
-    private static final String PATIENT_NHS_NUMBER_ID = "2.16.840.1.113883.2.1.4.1";
-
     private static final String NHS_NUMBER_SYSTEM_URL = "https://fhir.nhs.uk/Id/nhs-number";
-
+    private static final String META_PROFILE_URL = "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-Patient-1";
+    private static final String META_VERSION_ID = "1521806400000";
 
     public Patient mapToPatient(RCMRMT030101UK04Patient patient) {
-        var identifier = getIdentifier(patient.getId().getRoot());
-        var extension = patient.getId().getExtension();
-        return createPatient(identifier, extension);
+        var identifier = createIdentifier(patient.getId().getExtension());
+        return createPatient(identifier, null);
     }
 
+    public Patient mapToPatient(RCMRMT030101UK04Patient patient, Organization organization) {
+        var identifier = createIdentifier(patient.getId().getExtension());
+        var managingOrganization = createManagingOrganizationReference(organization);
+        return createPatient(identifier, managingOrganization);
+    }
 
-    private Identifier getIdentifier(String id) {
+    private Identifier createIdentifier(String nhsNumber) {
         return new Identifier()
-            .setSystem(IDENTIFIER_SYSTEM)
-            .setValue(id);
+            .setSystem(NHS_NUMBER_SYSTEM_URL)
+            .setValue(nhsNumber);
     }
 
-
-    private Patient createPatient(Identifier i, String extension) {
-        return new Patient()
-            .addIdentifier(i);
-
+    private Meta createMeta() {
+        return new Meta()
+            .setVersionId(META_VERSION_ID)
+            .addProfile(META_PROFILE_URL);
     }
 
+    private Reference createManagingOrganizationReference(Organization organization) {
+        return new Reference(organization.getIdElement());
+    }
+
+    private Patient createPatient(Identifier identifier, Reference managingOrganizationReference) {
+        return (Patient) new Patient()
+            .addIdentifier(identifier)
+            .setManagingOrganization(managingOrganizationReference)
+            .setMeta(createMeta());
+    }
 }
