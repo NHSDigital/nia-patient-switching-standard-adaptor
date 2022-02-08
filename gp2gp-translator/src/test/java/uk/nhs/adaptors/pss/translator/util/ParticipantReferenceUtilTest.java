c79d4cc0efdc633c3dc518a7c5725cd66cfc70a9
Barry Bell <gavin.bell@kainos.com>
2022-02-08 10:28:46 +0000

    Add unit tests

---
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/util/ParticipantReferenceUtilTest.java
@@ -0,0 +1,73 @@
+package uk.nhs.adaptors.pss.translator.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.util.ResourceUtils.getFile;
+
+import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallFile;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.hl7.v3.RCMRMT030101UK04EhrComposition;
+import org.hl7.v3.RCMRMT030101UK04Participant;
+import org.junit.jupiter.api.Test;
+
+import lombok.SneakyThrows;
+
+public class ParticipantReferenceUtilTest {
+    private static final String XML_RESOURCES_BASE = "xml/Participant/";
+
+    @SneakyThrows
+    private RCMRMT030101UK04EhrComposition unmarshallEhrCompositionElement(String fileName) {
+        return unmarshallFile(getFile("classpath:" + XML_RESOURCES_BASE + fileName), RCMRMT030101UK04EhrComposition.class);
+    }
+
+    private List<RCMRMT030101UK04Participant> getParticipants(RCMRMT030101UK04EhrComposition ehrComposition) {
+        return ehrComposition.getComponent().get(0).getPlanStatement().getParticipant();
+    }
+
+    @Test
+    public void mapResourceWithPprfParticipant() {
+        var ehrComposition = unmarshallEhrCompositionElement("pprf_participant.xml");
+        var participants = getParticipants(ehrComposition);
+
+        Reference participantReference = ParticipantReferenceUtil.getParticipantReference(participants, ehrComposition,
+            false);
+
+        assertThat(participantReference.getReference()).isEqualTo("Practitioner/8D1610C2-5E48-4ED5-882B-5A4A172AFA35");
+    }
+
+    @Test
+    public void mapResourceWithPrfParticipant() {
+        var ehrComposition = unmarshallEhrCompositionElement("prf_participant.xml");
+        var participants = getParticipants(ehrComposition);
+
+        Reference participantReference = ParticipantReferenceUtil.getParticipantReference(participants, ehrComposition,
+            false);
+
+        assertThat(participantReference.getReference()).isEqualTo("Practitioner/9C1610C2-5E48-4ED5-882B-5A4A172AFA35");
+    }
+
+    @Test
+    public void mapResourceWithParticipant2() {
+        var ehrComposition = unmarshallEhrCompositionElement("participant2_participant.xml");
+        var participants = getParticipants(ehrComposition);
+
+        Reference participantReference = ParticipantReferenceUtil.getParticipantReference(participants, ehrComposition,
+            false);
+
+        assertThat(participantReference.getReference()).isEqualTo("Practitioner/2D70F602-6BB1-47E0-B2EC-39912A59787D");
+    }
+
+    @Test
+    public void mapResourceWithNoValidParticipant() {
+        var ehrComposition = unmarshallEhrCompositionElement("participant2_participant.xml");
+        var participants = getParticipants(ehrComposition);
+
+        Reference participantReference = ParticipantReferenceUtil.getParticipantReference(participants, ehrComposition,
+            false);
+
+        assertThat(StringUtils.isEmpty(participantReference.getReference()));
+    }
+}
