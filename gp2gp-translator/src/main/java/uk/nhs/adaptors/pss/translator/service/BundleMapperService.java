1f7217facc90e9149524e4327ee5f36b1a03dc0c
bchotkowski <bartosz.chotkowski@kainos.com>
2022-02-09 15:40:34 +0100

    Continuing to make test with no Mockito

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/service/BundleMapperService.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/service/BundleMapperService.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.DomainResource;
+import org.hl7.fhir.dstu3.model.Location;
 import org.hl7.fhir.dstu3.model.Organization;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.ResourceType;
 import org.hl7.v3.RCMRIN030000UK06Message;
+import org.hl7.v3.RCMRMT030101UK04Component3;
 import org.hl7.v3.RCMRMT030101UK04Component4;
 import org.hl7.v3.RCMRMT030101UK04EhrExtract;
 import org.hl7.v3.RCMRMT030101UK04EhrFolder;
@@ -19,7 +22,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import uk.nhs.adaptors.pss.translator.generator.BundleGenerator;
 import uk.nhs.adaptors.pss.translator.mapper.AgentDirectoryMapper;
+import uk.nhs.adaptors.pss.translator.mapper.CodeableConceptMapper;
+import uk.nhs.adaptors.pss.translator.mapper.ConditionMapper;
+import uk.nhs.adaptors.pss.translator.mapper.DateTimeMapper;
+import uk.nhs.adaptors.pss.translator.mapper.LocationMapper;
 import uk.nhs.adaptors.pss.translator.mapper.PatientMapper;
+import uk.nhs.adaptors.pss.translator.mapper.ProcedureRequestMapper;
+import uk.nhs.adaptors.pss.translator.mapper.ReferralRequestMapper;
 
 @Slf4j
 @Service
@@ -28,37 +37,48 @@ public class BundleMapperService {
 
     private final BundleGenerator generator;
 
-    private final AgentDirectoryMapper agentDirectoryMapper;
     private final PatientMapper patientMapper;
+    private final AgentDirectoryMapper agentDirectoryMapper;
+    private final LocationMapper locationMapper;
+    private final ProcedureRequestMapper procedureRequestMapper;
+    private final ReferralRequestMapper referralRequestMapper;
 
     public Bundle mapToBundle(RCMRIN030000UK06Message xmlMessage) {
         LOGGER.info("Mapping the Bundle");
         Bundle bundle = generator.generateBundle();
+        final RCMRMT030101UK04EhrFolder ehrFolder = getEhrFolder(xmlMessage);
 
-        RCMRMT030101UK04EhrFolder ehrFolder = getEhrFolder(xmlMessage);
-        //List<RCMRMT030101UK04Component4> statementComponents = mapComponents(ehrFolder);
+     //   var agents = mapAgentDirectories(ehrFolder);
+     //   var patient = mapPatient(getEhrExtract(xmlMessage), findPatientOrganization(agents));
 
-        var agents = mapAgents(ehrFolder);
-
-
-        //bundle.addEntry(mapPatient(xmlMessage))
+     //   bundle.addEntry(patient);
+    //    agents.forEach(agent -> bundle.addEntry(new Bundle.BundleEntryComponent().setResource(agent)));
 
         return bundle;
     }
 
-    //    private List<Bundle.BundleEntryComponent> mapAgentDirectories(RCMRMT030101UK04EhrExtract ehrExtract) {
-    //       var ehrFolder = ehrExtract.getComponent().get(0).getEhrFolder();
-    //
-    //    }
-
-    private List<? extends DomainResource> mapAgents(RCMRMT030101UK04EhrFolder ehrFolder) {
+    private List<? extends DomainResource> mapAgentDirectories(RCMRMT030101UK04EhrFolder ehrFolder) {
         return agentDirectoryMapper.mapAgentDirectory(ehrFolder.getResponsibleParty().getAgentDirectory());
     }
 
-    private Bundle.BundleEntryComponent mapPatient(RCMRMT030101UK04EhrExtract ehrExtract, Organization organization) {
+    private List<Location> mapLocations(RCMRMT030101UK04EhrFolder ehrFolder) {
+        return ehrFolder.getComponent().stream()
+            .map(RCMRMT030101UK04Component3::getEhrComposition)
+            .filter(ehrComposition -> ehrComposition.getLocation() != null)
+            .map(e -> locationMapper.mapToLocation(e.getLocation(), e.getId().toString()))
+            .toList();
+    }
+
+    private Organization findPatientOrganization(List<? extends DomainResource> agents) {
+        return agents.stream()
+            .filter(agent -> ResourceType.Organization.equals(agent.getResourceType()))
+            .map(Organization.class::cast)
+            .findFirst().get();
+    }
+
+    private Patient mapPatient(RCMRMT030101UK04EhrExtract ehrExtract, Organization organization) {
         RCMRMT030101UK04Patient xmlPatient = ehrExtract.getRecordTarget().getPatient();
-        Patient mappedPatient = patientMapper.mapToPatient(xmlPatient, organization);
-        return new Bundle.BundleEntryComponent().setResource(mappedPatient);
+        return patientMapper.mapToPatient(xmlPatient, organization);
     }
 
     private List<RCMRMT030101UK04Component4> mapComponents(RCMRMT030101UK04EhrFolder ehrFolder) {
--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/service/BundleMapperServiceTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/service/BundleMapperServiceTest.java
@@ -1,41 +1,55 @@
 package uk.nhs.adaptors.pss.translator.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.util.ResourceUtils.getFile;
 
 import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallFile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.checkerframework.common.value.qual.MinLen;
 import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.DomainResource;
+import org.hl7.fhir.dstu3.model.Organization;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.v3.RCMRIN030000UK06Message;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.SneakyThrows;
 import uk.nhs.adaptors.pss.translator.generator.BundleGenerator;
+import uk.nhs.adaptors.pss.translator.mapper.AgentDirectoryMapper;
+import uk.nhs.adaptors.pss.translator.mapper.LocationMapper;
+import uk.nhs.adaptors.pss.translator.mapper.PatientMapper;
 
-@ExtendWith(MockitoExtension.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class BundleMapperServiceTest {
 
-    private final String XML_RESOURCES_BASE = "xml/RCMRIN030000UK06/";
+    private static final String XML_RESOURCES_BASE = "xml/RCMRIN030000UK06/";
 
-    @Mock
-    private BundleGenerator bundleGenerator;
-
-    @InjectMocks
+    @Autowired
     private BundleMapperService bundleMapperService;
 
     @Test
-    public void testAnything() {
-        when(bundleGenerator.generateBundle()).thenReturn(new Bundle());
+    public void testBundleIsCorrectlyMapped() {
         RCMRIN030000UK06Message xml = unmarshallCodeElement("structuredRecord.xml");
         Bundle bundle = bundleMapperService.mapToBundle(xml);
         assertThat(bundle.getEntry()).isEmpty();
+        //@TODO: Think of what to test later
     }
 
     @SneakyThrows
