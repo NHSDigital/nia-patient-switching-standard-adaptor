192e766fbb409c91355996567fcc9281f674a396
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2022-02-10 12:06:35 +0000

    reformatted testing

--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/ImmunizationMapperTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/ImmunizationMapperTest.java
@@ -5,11 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallFile;
 
-import org.hl7.fhir.dstu3.model.Encounter;
+import java.util.List;
+
 import org.hl7.fhir.dstu3.model.Identifier;
 import org.hl7.fhir.dstu3.model.Immunization;
-import org.hl7.fhir.dstu3.model.Patient;
-import org.hl7.fhir.dstu3.model.Practitioner;
 import org.hl7.v3.RCMRMT030101UK04EhrComposition;
 import org.hl7.v3.RCMRMT030101UK04ObservationStatement;
 import org.junit.jupiter.api.Test;
@@ -20,50 +19,64 @@ public class ImmunizationMapperTest {
     private static final String XML_RESOURCES_BASE = "xml/Immunization/";
     private static final String META_PROFILE = "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-Immunization-1";
     private static final String IDENTIFIER_SYSTEM = "https://PSSAdaptor/";
-    private static final String VACCINE_PROCEDURE_URL = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-VaccinationProcedure-1";
 
     private final ImmunizationMapper immunizationMapper = new ImmunizationMapper();
 
 
     @Test
     public void mapObservationToImmunizationWithValidData() {
-        var observationStatement = unmarshallCodeElement("full_valid_immunization.xml");
-        immunizationMapper.mapToImmunization(observationStatement, "655D5A78-1F63-434C-9637-1D7E7843341B",
-            "655D5A78-1F63-434C-9637-1D7E7843341B", new RCMRMT030101UK04EhrComposition()).forEach(
-                immunization -> assertFullValidData(observationStatement, immunization)
-        );
+        var ehrComposition = unmarshallEhrComposition("full_valid_immunization.xml");
+        var observationStatement = ehrComposition.getComponent().get(0).getObservationStatement();
+        List immunizationList = immunizationMapper.mapToImmunization(observationStatement, "9A5D5A78-1F63-434C-9637-1D7E7843341B",
+            "655D5A78-1F63-434C-9637-1D7E7843341B", ehrComposition);
+        var immunization = (Immunization) immunizationList.get(0);
+
+        assertFullValidData(observationStatement, immunization);
     }
 
     @Test
     public void mapObservationToImmunizationWithEffectiveTimeCenter() {
-        var observationStatement = unmarshallCodeElement("immunization_with_only_center_effective_time.xml");
-        immunizationMapper.mapToImmunization(observationStatement, "655D5A78-1F63-434C-9637-1D7E7843341B",
-            "655D5A78-1F63-434C-9637-1D7E7843341B", new RCMRMT030101UK04EhrComposition()).forEach(immunization ->
-            assertImmunizationWithHighEffectiveTimeCenter(immunization, observationStatement));
+        var ehrComposition = unmarshallEhrComposition("immunization_with_only_center_effective_time.xml");
+        var observationStatement = ehrComposition.getComponent().get(0).getObservationStatement();
+        List immunizationList = immunizationMapper.mapToImmunization(observationStatement, "9A5D5A78-1F63-434C-9637-1D7E7843341B",
+            "655D5A78-1F63-434C-9637-1D7E7843341B", ehrComposition);
+        var immunization = (Immunization) immunizationList.get(0);
+
+        assertImmunizationWithHighEffectiveTimeCenter(immunization, observationStatement);
     }
 
     @Test
     public void mapObservationToImmunizationWithEffectiveTimeLow() {
-        var observationStatement = unmarshallCodeElement("immunization_with_only_low_effective_time.xml");
-        immunizationMapper.mapToImmunization(observationStatement, "655D5A78-1F63-434C-9637-1D7E7843341B",
-            "655D5A78-1F63-434C-9637-1D7E7843341B", new RCMRMT030101UK04EhrComposition()).forEach(
-                immunization -> assertImmunizationWithEffectiveTimeLow(immunization, observationStatement));
+        var ehrComposition = unmarshallEhrComposition("immunization_with_only_low_effective_time.xml");
+        var observationStatement = ehrComposition.getComponent().get(0).getObservationStatement();
+        List immunizationList = immunizationMapper.mapToImmunization(observationStatement, "9A5D5A78-1F63-434C-9637-1D7E7843341B",
+            "655D5A78-1F63-434C-9637-1D7E7843341B", ehrComposition);
+        var immunization = (Immunization) immunizationList.get(0);
+
+        assertImmunizationWithEffectiveTimeLow(immunization, observationStatement);
     }
 
     @Test
     public void mapObservationToImmunizationWithHighAndLowEffectiveTime() {
-        var observationStatement = unmarshallCodeElement("immunization_with_high_and_low_effective_time.xml");
-        immunizationMapper.mapToImmunization(observationStatement, "655D5A78-1F63-434C-9637-1D7E7843341B",
-            "655D5A78-1F63-434C-9637-1D7E7843341B", new RCMRMT030101UK04EhrComposition()).forEach(
-                immunization -> assertImmunizationWithHighAndLowEffectiveTime(immunization, observationStatement));
+        var ehrComposition = unmarshallEhrComposition("immunization_with_high_and_low_effective_time.xml");
+        var observationStatement = ehrComposition.getComponent().get(0).getObservationStatement();
+        List immunizationList = immunizationMapper.mapToImmunization(observationStatement, "9A5D5A78-1F63-434C-9637-1D7E7843341B",
+            "655D5A78-1F63-434C-9637-1D7E7843341B", ehrComposition);
+        var immunization = (Immunization) immunizationList.get(0);
+
+        assertImmunizationWithHighAndLowEffectiveTime(immunization, observationStatement);
     }
 
     @Test
     public void mapObservationToImmunizationWithHighEffectiveTime() {
-        var observationStatement = unmarshallCodeElement("immunization_with_only_high_effective_time.xml");
-        immunizationMapper.mapToImmunization(observationStatement, "655D5A78-1F63-434C-9637-1D7E7843341B",
-            "655D5A78-1F63-434C-9637-1D7E7843341B", new RCMRMT030101UK04EhrComposition()).forEach(
-                immunization -> assertImmunizationWithHighEffectiveTime(immunization, observationStatement));
+
+        var ehrComposition = unmarshallEhrComposition("immunization_with_only_high_effective_time.xml");
+        var observationStatement = ehrComposition.getComponent().get(0).getObservationStatement();
+        List immunizationList = immunizationMapper.mapToImmunization(observationStatement, "9A5D5A78-1F63-434C-9637-1D7E7843341B",
+            "655D5A78-1F63-434C-9637-1D7E7843341B", ehrComposition);
+        var immunization = (Immunization) immunizationList.get(0);
+
+        assertImmunizationWithHighEffectiveTime(immunization, observationStatement);
     }
 
     private void assertImmunizationWithHighAndLowEffectiveTime(Immunization immunization,
@@ -106,6 +119,8 @@ public class ImmunizationMapperTest {
         assertThat(immunization.getNote().get(0).getText())
             .isEqualTo(observationStatement.getPertinentInformation().get(0).getPertinentAnnotation().getText()
                 + " End Date: 20100118114100");
+        assertThat(immunization.getPatient().getReference()).isEqualTo("Patient/9A5D5A78-1F63-434C-9637-1D7E7843341B");
+        assertThat(immunization.getEncounter().getReference()).isEqualTo("Encounter/655D5A78-1F63-434C-9637-1D7E7843341B");
     }
 
     private void assertThatIdentifierIsValid(Identifier identifier, String id) {
@@ -114,9 +129,7 @@ public class ImmunizationMapperTest {
     }
 
     @SneakyThrows
-    private RCMRMT030101UK04ObservationStatement unmarshallCodeElement(String fileName) {
-        return unmarshallFile(getFile("classpath:" + XML_RESOURCES_BASE + fileName), RCMRMT030101UK04ObservationStatement.class);
+    private RCMRMT030101UK04EhrComposition unmarshallEhrComposition(String fileName) {
+        return unmarshallFile(getFile("classpath:" + XML_RESOURCES_BASE + fileName), RCMRMT030101UK04EhrComposition.class);
     }
-
-
 }
