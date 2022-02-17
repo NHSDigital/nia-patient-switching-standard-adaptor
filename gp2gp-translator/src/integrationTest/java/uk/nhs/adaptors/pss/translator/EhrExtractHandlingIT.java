6c37094e50325eeec8338d5188f7747f2670f795
mrogowska <m.rogowska@kainos.com>
2022-02-17 10:14:27 +0100

    NIAD-1881: Upgrade Awaitility lib

--- gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/EhrExtractHandlingIT.java
+++ gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/EhrExtractHandlingIT.java
@@ -1,8 +1,7 @@
 package uk.nhs.adaptors.pss.translator;
 
+import static org.awaitility.Awaitility.await;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-import static com.jayway.awaitility.Awaitility.await;
 
 import static uk.nhs.adaptors.common.testutil.FileUtil.readResourceAsString;
 import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_ACCEPTED;
