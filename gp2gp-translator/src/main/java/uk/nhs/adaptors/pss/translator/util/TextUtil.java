9f09492e5e03d370fe24122be2c742b85b906e59
bchotkowski <bartosz.chotkowski@kainos.com>
2022-03-03 09:33:31 +0100

    Secured utils from one-line or empty string, added utils test

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/TextUtil.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/TextUtil.java
@@ -7,7 +7,8 @@ import org.apache.commons.lang3.StringUtils;
 public class TextUtil {
 
     public static String getLastLine(String text) {
-        return deleteWhitespace(text.substring(text.lastIndexOf(StringUtils.LF)));
+        return !text.contains(StringUtils.LF)
+            ? text : deleteWhitespace(text.substring(text.lastIndexOf(StringUtils.LF)));
     }
 
 }
---
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/util/TextUtilTest.java
@@ -0,0 +1,49 @@
+package uk.nhs.adaptors.pss.translator.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static uk.nhs.adaptors.pss.translator.util.TextUtil.getLastLine;
+
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TextUtilTest {
+
+    private static final String THREE_LINER = """
+        HELLO
+        PS
+        ADAPTER""";
+
+    private static final String TWO_LINER = """
+        HELLO
+        WORLD""";
+
+    private static final String ONE_LINER = "HELLO";
+
+    private static final String SPATIAL_THREE_LINER = """
+        HELLO            
+            PS            
+                 ADAPTER       """;
+
+    @ParameterizedTest
+    @MethodSource("testInputs")
+    public void testGettingOneLiner(String input, String expected) {
+        String line = getLastLine(input);
+        assertThat(line).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> testInputs() {
+        return Stream.of(
+            Arguments.of(ONE_LINER, ONE_LINER),
+            Arguments.of(TWO_LINER, "WORLD"),
+            Arguments.of(THREE_LINER, "ADAPTER"),
+            Arguments.of(SPATIAL_THREE_LINER, "ADAPTER"),
+            Arguments.of(StringUtils.EMPTY, StringUtils.EMPTY)
+        );
+    }
+
+}
