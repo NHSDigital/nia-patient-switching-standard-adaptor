291fa81be783115e0c259b57ced91d780dd62ca7
AlanSimpson1723 <56081521+AlanSimpson1723@users.noreply.github.com>
2022-03-04 13:52:09 +0000

    implemented mapper and testing

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/DocumentReferenceMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/DocumentReferenceMapper.java
@@ -1,32 +1,50 @@
 package uk.nhs.adaptors.pss.translator.mapper;
 
+import static uk.nhs.adaptors.pss.translator.util.ResourceUtil.buildIdentifier;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.dstu3.model.Attachment;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.DocumentReference;
+import org.hl7.fhir.dstu3.model.Encounter;
 import org.hl7.fhir.dstu3.model.Enumerations;
-import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.InstantType;
+import org.hl7.fhir.dstu3.model.Organization;
+import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.v3.RCMRMT030101UK04Component4;
 import org.hl7.v3.RCMRMT030101UK04EhrComposition;
 import org.hl7.v3.RCMRMT030101UK04EhrExtract;
+import org.hl7.v3.RCMRMT030101UK04NarrativeStatement;
 import org.springframework.stereotype.Service;
 
-import org.hl7.v3.RCMRMT030101UK04NarrativeStatement;
-
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import uk.nhs.adaptors.pss.translator.util.DateFormatUtil;
 import uk.nhs.adaptors.pss.translator.util.EhrResourceExtractorUtil;
+import uk.nhs.adaptors.pss.translator.util.EncounterReferenceUtil;
+import uk.nhs.adaptors.pss.translator.util.ParticipantReferenceUtil;
 
+@Slf4j
 @Service
+@AllArgsConstructor
 public class DocumentReferenceMapper {
-    private static final String IDENTIFIER_SYSTEM = "https://PSSAdaptor/";
     private static final String META_PROFILE = "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-DocumentReference-1";
+    private static final String ABSENT_ATTACHMENT = "Absent Attachment";
+    private static final String PLACEHOLDER_VALUE = "GP2GP generated placeholder. Original document not available. See notes for details";
+    private static final String INVALID_CONTENT_TYPE = "Content type was not a valid MIME type";
 
     private CodeableConceptMapper codeableConceptMapper;
 
-    public List<DocumentReference> mapToDocumentReference(RCMRMT030101UK04EhrExtract ehrExtract) {
+
+    public List<DocumentReference> mapToDocumentReference(RCMRMT030101UK04EhrExtract ehrExtract, Patient patient,
+        Organization organization, Long fileSize, List<Encounter> encounterList, String practiseCode) {
         List<DocumentReference> mappedDocumentReferenceResources = new ArrayList<>();
         var ehrCompositionList = EhrResourceExtractorUtil.extractValidDocumentReferenceEhrCompositions(ehrExtract);
 
@@ -35,7 +53,8 @@ public class DocumentReferenceMapper {
 
             narrativeStatements
                 .forEach(narrativeStatement -> {
-                    var mappedDocumentReferences = mapDocumentReference(narrativeStatement);
+                    var mappedDocumentReferences = mapDocumentReference(narrativeStatement, patient, ehrExtract,
+                        organization, fileSize, encounterList, practiseCode);
                     mappedDocumentReferenceResources.add(mappedDocumentReferences);
                 });
         });
@@ -43,17 +62,45 @@ public class DocumentReferenceMapper {
         return mappedDocumentReferenceResources;
     }
 
-    private DocumentReference mapDocumentReference(RCMRMT030101UK04NarrativeStatement narrativeStatement) {
+    private DocumentReference mapDocumentReference(RCMRMT030101UK04NarrativeStatement narrativeStatement, Patient patient,
+        RCMRMT030101UK04EhrExtract ehrExtract, Organization organization, long fileSize,
+        List<Encounter> encounterList, String practiseCode) {
         DocumentReference documentReference = new DocumentReference();
 
-        var id = narrativeStatement.getId().getRoot();
-        var identifier = getIdentifier(id);
+        var ehrComposition = ehrExtract.getComponent().get(0).getEhrFolder().getComponent().get(0).getEhrComposition();
 
-        documentReference.addIdentifier(identifier);
+        var id = narrativeStatement.getReference().get(0).getReferredToExternalDocument().getId().getRoot();
+
+        documentReference.addIdentifier(buildIdentifier(id, practiseCode));
         documentReference.getMeta().addProfile(META_PROFILE);
         documentReference.setStatus(Enumerations.DocumentReferenceStatus.CURRENT);
         documentReference.setId(id);
-        documentReference.setType(getType(narrativeStatement, documentReference));
+        documentReference.setType(getType(narrativeStatement));
+        documentReference.setSubject(new Reference(patient));
+        documentReference.setIndexedElement(getIndexed(ehrExtract));
+        documentReference.setAuthor(getAuthor(narrativeStatement, ehrComposition));
+        documentReference.setDescription(buildDescription(narrativeStatement));
+
+        if (organization != null && organization.hasIdElement()) {
+            documentReference.setCustodian(createOrganizationReference(organization));
+        }
+
+        if (narrativeStatement.getAvailabilityTime() != null && narrativeStatement.getAvailabilityTime().getValue() != null) {
+            documentReference.setCreatedElement(
+                DateFormatUtil.parseToDateTimeType(narrativeStatement.getAvailabilityTime().getValue()));
+        }
+
+        var encounterReference =
+            EncounterReferenceUtil.getEncounterReference(List.of(ehrComposition), encounterList, ehrComposition.getId().getRoot());
+
+        if (encounterReference != null) {
+            DocumentReference.DocumentReferenceContextComponent documentReferenceContextComponent
+                = new DocumentReference.DocumentReferenceContextComponent().setEncounter(encounterReference);
+
+            documentReference.setContext(documentReferenceContextComponent);
+        }
+
+        setContentAttachments(documentReference, narrativeStatement, fileSize);
 
         return documentReference;
     }
@@ -66,26 +113,110 @@ public class DocumentReferenceMapper {
             .collect(Collectors.toList());
     }
 
-    private Identifier getIdentifier(String id) {
-        return new Identifier()
-            .setSystem(IDENTIFIER_SYSTEM) // TODO: concatenate source practice org id to URL (NIAD-2021)
-            .setValue(id);
-    }
-
-    private CodeableConcept getType(RCMRMT030101UK04NarrativeStatement narrativeStatement, DocumentReference documentReference) {
+    private CodeableConcept getType(RCMRMT030101UK04NarrativeStatement narrativeStatement) {
         var referenceToExternalDocument = narrativeStatement.getReference().get(0).getReferredToExternalDocument();
 
         if (referenceToExternalDocument != null && referenceToExternalDocument.getCode() != null) {
             if (referenceToExternalDocument.getCode().getOriginalText() == null
                 && referenceToExternalDocument.getCode().getDisplayName() != null) {
-                //documentReference.setText();
-                return codeableConceptMapper.mapToCodeableConcept(referenceToExternalDocument.getCode());
+                return codeableConceptMapper.mapToCodeableConcept(referenceToExternalDocument.getCode())
+                    .setText(referenceToExternalDocument.getCode().getDisplayName());
             } else if (referenceToExternalDocument.getCode().getOriginalText() != null) {
-                //documentReference.setText();
-                return codeableConceptMapper.mapToCodeableConcept(referenceToExternalDocument.getCode());
+                return codeableConceptMapper.mapToCodeableConcept(referenceToExternalDocument.getCode())
+                    .setText(referenceToExternalDocument.getCode().getOriginalText());
             }
         }
 
         return null;
+    }
+
+    private InstantType getIndexed(RCMRMT030101UK04EhrExtract ehrExtract) {
+        if (ehrExtract.getAuthor() != null) {
+            if (ehrExtract.getAuthor().getTime() != null && ehrExtract.getAuthor().getNullFlavor() == null) {
+                return DateFormatUtil.parseToInstantType(ehrExtract.getAuthor().getTime().getValue());
+            }
+
+            return DateFormatUtil.parseToInstantType(ehrExtract.getAvailabilityTime().getValue());
+        }
+
+        return null;
+    }
+
+    private List<Reference> getAuthor(RCMRMT030101UK04NarrativeStatement narrativeStatement,
+        RCMRMT030101UK04EhrComposition ehrComposition) {
+        return List.of(ParticipantReferenceUtil.getParticipantReference(narrativeStatement.getParticipant(), ehrComposition));
+    }
+
+    private Reference createOrganizationReference(Organization organization) {
+        return new Reference(organization.getIdElement());
+    }
+
+    private String buildDescription(RCMRMT030101UK04NarrativeStatement narrativeStatement) {
+        var referenceToExternalDocumentText = narrativeStatement.getReference().get(0).getReferredToExternalDocument().getText();
+        if (isAbsentAttachment(narrativeStatement)) {
+            return PLACEHOLDER_VALUE;
+        }
+        if (referenceToExternalDocumentText != null) {
+            String filename = referenceToExternalDocumentText.getReference().getValue();
+            if (filename != null) {
+                return buildFileName(filename);
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isAbsentAttachment(RCMRMT030101UK04NarrativeStatement narrativeStatement) {
+        return narrativeStatement.getReference().get(0)
+            .getReferredToExternalDocument()
+            .getCode()
+            .getOriginalText()
+            .equals(ABSENT_ATTACHMENT);
+    }
+
+    private void setContentAttachments(DocumentReference documentReference, RCMRMT030101UK04NarrativeStatement narrativeStatement,
+        long fileSize) {
+        var referenceToExternalDocument = narrativeStatement.getReference().get(0).getReferredToExternalDocument();
+        var attachment = new Attachment();
+        if (referenceToExternalDocument.getText() != null) {
+            var mediaType = referenceToExternalDocument.getText().getMediaType();
+
+            if (!isAbsentAttachment(narrativeStatement)) {
+                attachment.setUrl(referenceToExternalDocument.getText().getReference().getValue());
+                attachment.setSize((int) fileSize);
+                attachment.setTitle(buildFileName(referenceToExternalDocument.getText().getReference().getValue()));
+            } else {
+                attachment.setTitle(PLACEHOLDER_VALUE);
+            }
+
+            if (isContentTypeValid(mediaType)) {
+                attachment.setContentType(mediaType);
+            } else {
+                attachment.setContentType(PLACEHOLDER_VALUE);
+                addContentTypeToNotes(documentReference);
+                LOGGER.info("Content type was not a valid MIME type");
+            }
+
+            documentReference.addContent(new DocumentReference.DocumentReferenceContentComponent(attachment));
+        }
+    }
+
+    private void addContentTypeToNotes(DocumentReference documentReference) {
+        if (documentReference.getDescription().isEmpty()) {
+            documentReference.setDescription(INVALID_CONTENT_TYPE);
+        } else {
+            String previousDesc = documentReference.getDescription();
+            String newDesc = previousDesc + StringUtils.SPACE + INVALID_CONTENT_TYPE;
+            documentReference.setDescription(newDesc);
+        }
+    }
+
+    private String buildFileName(String text) {
+        return text.replaceAll("file://localhost/", "Filename: ");
+    }
+
+    private boolean isContentTypeValid(String mediaType) {
+        String validContentTypeFormat = ".*/.*";
+        return Pattern.matches(validContentTypeFormat, mediaType);
     }
 }
--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/DocumentReferenceTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/DocumentReferenceTest.java
@@ -1,17 +1,33 @@
 package uk.nhs.adaptors.pss.translator.mapper;
 
+import static java.util.UUID.randomUUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.util.ResourceUtils.getFile;
 
 import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.DocumentReference;
+import org.hl7.fhir.dstu3.model.Encounter;
+import org.hl7.fhir.dstu3.model.Enumerations;
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Organization;
+import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.v3.RCMRMT030101UK04EhrExtract;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import lombok.SneakyThrows;
@@ -19,22 +35,167 @@ import lombok.SneakyThrows;
 @ExtendWith(MockitoExtension.class)
 public class DocumentReferenceTest {
     private static final String XML_RESOURCES_BASE = "xml/DocumentReference/";
-    private static final String NARRATIVE_STATEMENT_ROOT_ID = "5E496953-065B-41F2-9577-BE8F2FBD0757";
+    private static final String NARRATIVE_STATEMENT_ROOT_ID = "31B75ED0-6E88-11EA-9384-E83935108FD5";
+    private static final String ORGANIZATION_ID = "CBF2EC6E-EC9E-40FF-B098-5FBA732F8927";
+    private static final String META_PROFILE = "https://fhir.nhs.uk/STU3/StructureDefinition/CareConnect-GPC-DocumentReference-1";
+    private static final String CODING_DISPLAY = "Original Text document";
+    private static final String IDENTIFIER_SYSTEM = "https://PSSAdaptor/TESTPRACTISECODE";
+    private static final String NARRATIVE_STATEMENT_TYPE = "Record Attachment";
+    private static final String FILENAME = "Filename: 31B75ED0-6E88-11EA-9384-E83935108FD5_patient-attachment.txt";
+    private static final String URL = "file://localhost/31B75ED0-6E88-11EA-9384-E83935108FD5_patient-attachment.txt";
+    private static final String CONTENT_TYPE = "text/plain";
+    private static final String ENCOUNTER_ID = "62A39454-299F-432E-993E-5A6232B4E099";
+    private static final String PRACTISE_CODE = "TESTPRACTISECODE";
+    private static final String PLACEHOLDER = "GP2GP generated placeholder. Original document not available. See notes for details";
+    private static final int THREE = 3;
+    private static final int TWO_SIX_FIVE_TWO = 2652;
 
     @InjectMocks
     private DocumentReferenceMapper documentReferenceMapper;
 
+    @Mock
+    private CodeableConceptMapper codeableConceptMapper;
+
+    @BeforeEach
+    public void setup() {
+        setUpCodeableConceptMock();
+    }
+
     @Test
     public void mapNarrativeStatementToDocumentReferenceWithValidData() {
-        var ehrExtract = unmarshallEhrExtract("narrative_statement_has_referred_to_external_document.xml");
-        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract);
+        String filename = "narrative_statement_has_referred_to_external_document.xml";
+        var ehrExtract = unmarshallEhrExtract(filename);
+        var fileSize = getFileSize(filename);
+        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract, createPatient(),
+            createOrganization(), fileSize, getEncounterList(), PRACTISE_CODE);
         var documentReference = documentReferences.get(0);
 
         assertFullValidData(documentReference);
     }
 
+    @Test
+    public void mapNarrativeStatementToDocumentReferenceWithOptionalData() {
+        String filename = "narrative_statement_has_referred_to_external_document_with_optional_data.xml";
+        var ehrExtract = unmarshallEhrExtract(filename);
+        var fileSize = getFileSize(filename);
+        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract, createPatient(),
+            createOrganization(), fileSize, getEncounterList(), PRACTISE_CODE);
+
+        var documentReference = documentReferences.get(0);
+
+        assertOptionalValidData(documentReference);
+    }
+
+    @Test
+    public void mapMultpleNarrativeStatementToDocumentReference() {
+        String filename = "multiple_narrative_statements_has_referred_to_external_document.xml";
+        var ehrExtract = unmarshallEhrExtract(filename);
+        var fileSize = getFileSize(filename);
+        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract, createPatient(),
+            createOrganization(), fileSize, getEncounterList(), PRACTISE_CODE);
+
+        assertThat(documentReferences.size()).isEqualTo(THREE);
+    }
+
+    @Test
+    public void mapNarrativeStatementToDocumentReferenceWithAttachments() {
+        String filename = "narrative_statement_has_referred_to_external_document.xml";
+        var ehrExtract = unmarshallEhrExtract(filename);
+        var fileSize = getFileSize(filename);
+        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract, createPatient(),
+            createOrganization(), fileSize, getEncounterList(), PRACTISE_CODE);
+        var documentReference = documentReferences.get(0);
+
+        assertAttachmentData(documentReference);
+    }
+
+    @Test
+    public void mapNarrativeStatementToDocumentReferenceWithAbsentAttachment() {
+        String filename = "narrative_statement_has_referred_to_external_document_with_absent_attachment.xml";
+        var ehrExtract = unmarshallEhrExtract(filename);
+        var fileSize = getFileSize(filename);
+        List<DocumentReference> documentReferences = documentReferenceMapper.mapToDocumentReference(ehrExtract, createPatient(),
+            createOrganization(), fileSize, getEncounterList(), PRACTISE_CODE);
+        var documentReference = documentReferences.get(0);
+
+        assertDocumentReferenceWithAbsentAttachment(documentReference);
+    }
+
     private void assertFullValidData(DocumentReference documentReference) {
         assertThat(documentReference.getId()).isEqualTo(NARRATIVE_STATEMENT_ROOT_ID);
+        assertThat(documentReference.getMeta().getProfile().get(0).getValue()).isEqualTo(META_PROFILE);
+        assertThat(documentReference.getStatus()).isEqualTo(Enumerations.DocumentReferenceStatus.CURRENT);
+        assertThatIdentifierIsValid(documentReference.getIdentifierFirstRep(), documentReference.getId());
+        assertThat(documentReference.getType().getText()).isEqualTo(NARRATIVE_STATEMENT_TYPE);
+        assertThat(documentReference.getAuthor().get(0).getReference()).isEqualTo("Practitioner/2D70F602-6BB1-47E0-B2EC-39912A59787D");
+        assertThat(documentReference.getDescription()).isEqualTo("Filename: 31B75ED0-6E88-11EA-9384-E83935108FD5_patient-attachment.txt");
+        assertThat(documentReference.getIndexedElement().getValue().toInstant().toString()).isEqualTo("2010-01-14T00:00:00Z");
+        assertThat(documentReference.getCustodian().getReference()).isEqualTo(ORGANIZATION_ID);
+        assertThat(documentReference.getCreatedElement().getValue().toInstant().toString()).isEqualTo("2020-10-12T13:33:44Z");
+    }
+
+    private void assertOptionalValidData(DocumentReference documentReference) {
+        assertThat(documentReference.getId()).isEqualTo(NARRATIVE_STATEMENT_ROOT_ID);
+        assertThat(documentReference.getMeta().getProfile().get(0).getValue()).isEqualTo(META_PROFILE);
+        assertThat(documentReference.getStatus()).isEqualTo(Enumerations.DocumentReferenceStatus.CURRENT);
+        assertThatIdentifierIsValid(documentReference.getIdentifierFirstRep(), documentReference.getId());
+        assertThat(documentReference.getType().getText()).isEqualTo(NARRATIVE_STATEMENT_TYPE);
+        assertThat(documentReference.getAuthor().get(0).getReference()).isEqualTo("Practitioner/2D70F602-6BB1-47E0-B2EC-39912A59787D");
+        assertThat(documentReference.getDescription()).isNull();
+        assertThat(documentReference.getCreated()).isNull();
+        assertThat(documentReference.getContent()).isEmpty();
+    }
+
+    private void assertAttachmentData(DocumentReference documentReference) {
+        assertThat(documentReference.getContent().get(0).getAttachment().getSize()).isEqualTo(TWO_SIX_FIVE_TWO);
+        assertThat(documentReference.getContent().get(0).getAttachment().getTitle()).isEqualTo(FILENAME);
+        assertThat(documentReference.getContent().get(0).getAttachment().getUrl()).isEqualTo(URL);
+        assertThat(documentReference.getContent().get(0).getAttachment().getContentType()).isEqualTo(CONTENT_TYPE);
+    }
+
+    private void assertDocumentReferenceWithAbsentAttachment(DocumentReference documentReference) {
+        assertThat(documentReference.getId()).isEqualTo(NARRATIVE_STATEMENT_ROOT_ID);
+        assertThat(documentReference.getContent().get(0).getAttachment().getTitle()).isEqualTo(PLACEHOLDER);
+        assertThat(documentReference.getContent().get(0).getAttachment().getSize()).isEqualTo(0);
+        assertThat(documentReference.getContent().get(0).getAttachment().getUrl()).isNull();
+        assertThat(documentReference.getContent().get(0).getAttachment().getContentType()).isEqualTo(CONTENT_TYPE);
+    }
+
+    private void assertThatIdentifierIsValid(Identifier identifier, String id) {
+        assertThat(identifier.getSystem()).isEqualTo(IDENTIFIER_SYSTEM);
+        assertThat(identifier.getValue()).isEqualTo(id);
+    }
+
+    private static Patient createPatient() {
+        Patient patient = new Patient();
+        patient.setId(randomUUID().toString());
+        return patient;
+    }
+
+    private void setUpCodeableConceptMock() {
+        var codeableConcept = new CodeableConcept();
+        var coding = new Coding();
+        coding.setDisplay(CODING_DISPLAY);
+        codeableConcept.addCoding(coding);
+        when(codeableConceptMapper.mapToCodeableConcept(any())).thenReturn(codeableConcept);
+    }
+
+    private List<Encounter> getEncounterList() {
+        var encounter = new Encounter();
+        encounter.setId(ENCOUNTER_ID);
+        return List.of(encounter);
+    }
+
+    private static Organization createOrganization() {
+        Organization organization = new Organization();
+        organization.setId(ORGANIZATION_ID);
+        return organization;
+    }
+
+    @SneakyThrows
+    private long getFileSize(String filename) {
+        File file = getFile("classpath:" + XML_RESOURCES_BASE + filename);
+        return Files.size(Path.of(file.getPath()));
     }
 
     @SneakyThrows
