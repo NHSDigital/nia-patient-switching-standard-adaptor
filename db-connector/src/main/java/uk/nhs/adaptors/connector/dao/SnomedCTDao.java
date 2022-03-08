2a54b2413a05c7cd1ddad0c2784068dfe6c90b8a
Barry Bell <gavin.bell@kainos.com>
2022-03-08 10:37:37 +0000

    Initial development work

---
+++ db-connector/src/main/java/uk/nhs/adaptors/connector/dao/SnomedCTDao.java
@@ -0,0 +1,11 @@
+package uk.nhs.adaptors.connector.dao;
+
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.locator.UseClasspathSqlLocator;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
+import uk.nhs.adaptors.connector.model.MigrationStatusLog;
+
+public interface SnomedCTDao {
+    
+}
--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/CodeableConceptMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/CodeableConceptMapper.java
@@ -1,5 +1,6 @@
 package uk.nhs.adaptors.pss.translator.mapper;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.v3.CD;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,14 @@ import org.springframework.stereotype.Service;
 public class CodeableConceptMapper {
     private static final String SNOMED_SYSTEM_CODE = "2.16.840.1.113883.2.1.3.2.4.15";
     private static final String SNOMED_SYSTEM = "http://snomed.info/sct";
+    private static final String CONCEPT_PARTITION_SHORT = "00";
+    private static final String CONCEPT_PARTITION_LONG = "10";
+    private static final String DESCRIPTION_PARTITION_SHORT = "01";
+    private static final String DESCRIPTION_PARTITION_LONG = "11";
 
     public CodeableConcept mapToCodeableConcept(CD codedData) {
         if (hasSnomedCodeInMainCode(codedData)) {
+
             return createCodeableConcept(codedData.getCode(), SNOMED_SYSTEM, codedData.getDisplayName(), codedData.getOriginalText());
         } else if (hasSnomedCodeInTranslationElement(codedData)) {
             var translation = getSnomedTranslationElement(codedData);
@@ -20,6 +26,38 @@ public class CodeableConceptMapper {
 
         var text = getText(codedData);
         return new CodeableConcept().setText(text);
+    }
+
+    private CodeableConcept generateSnomedCodeableConcept(String code, String displayName) {
+        var partitionIdentifier = getPartitionIdentifier(code);
+
+        if (hasConceptId(partitionIdentifier)) {
+            if (StringUtils.isNotEmpty(displayName)) {
+                // obtain description id from db
+                //// if description is preferred term then there is no synonym 
+                ////// we output concept id as .code and displayName as .display and description id as description id in the extension
+                
+                //// if description is NOT preferred term then there IS synonym 
+                ////// we output description id of desc matching displayName and the dislayName as the .descriptionId and .descriptionId in the extension. Term text of preferred term as .code and .display
+            
+                //// if no matching description get displayName use preferred term (see spread sheet)            
+            }
+        } else if (hasDescriptionId(partitionIdentifier)) {
+                // obtain descriptionId from using concept id and seect the preferred term
+                //// description id is used in the extension (https://fhir.nhs.uk/STU3/StructureDefinition/Extension-coding-sctdescid)
+                //// term text as .display
+                //// the code used for concept is used as .code
+            
+                //// if snomed coding is obtained from translation and displayName is absent, obtain display name from main code and compare to preferred text (see spreadsheet) 
+        }
+
+        // if description id
+        //// obtain description from db
+        ////// if the preferreD term
+        //////// output the description id in the extension
+        //////// output term text from descrpiotn as .display and concept id from descrpion as .code in CC
+        
+        return null;
     }
 
     private boolean hasSnomedCodeInMainCode(CD codedData) {
@@ -46,6 +84,20 @@ public class CodeableConceptMapper {
 
     private String getDisplayForSnomedTranslationElement(CD codedData, CD translation) {
         return translation.getDisplayName() != null ? translation.getDisplayName() : codedData.getDisplayName();
+    }
+
+    private boolean hasConceptId(String partitionIdentifier) {
+        return CONCEPT_PARTITION_SHORT.equals(partitionIdentifier) || CONCEPT_PARTITION_LONG.equals(partitionIdentifier);
+    }
+
+    private boolean hasDescriptionId(String partitionIdentifier) {
+        return DESCRIPTION_PARTITION_SHORT.equals(partitionIdentifier) || DESCRIPTION_PARTITION_LONG.equals(partitionIdentifier);
+    }
+
+    private String getPartitionIdentifier(String code) {
+        var codeLength = code.length();
+
+        return code.substring(code.length() - 3, code.length() - 1);
     }
 
     private CodeableConcept createCodeableConcept(String code, String system, String display, String text) {
