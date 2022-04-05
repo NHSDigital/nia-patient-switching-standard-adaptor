cfe1cf6c28e2d73b13e764594a25ffe12dab0fc7
Ben Hession <bshession@gmail.com>
2022-04-05 18:30:49 +0100

    Remove unnecessary instance variables.

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/model/InlineAttachment.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/model/InlineAttachment.java
@@ -1,5 +1,6 @@
 package uk.nhs.adaptors.pss.translator.model;
 
+import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,24 +12,20 @@ public class InlineAttachment {
     private final String originalFilename;
     private final String contentType;
     private final boolean isCompressed;
-    private final boolean isLargeAttachment;
-    private final boolean isOriginalBase64;
     private final boolean isBase64;
     private final String description;
     private final String payload;
 
-    public InlineAttachment(InboundMessage.Attachment attachment) {
+    public InlineAttachment(InboundMessage.Attachment attachment) throws ParseException {
         this.originalFilename = parseFilename(attachment.getDescription());
         this.contentType = attachment.getContentType();
         this.isCompressed = parseCompressed(attachment.getDescription());
-        this.isLargeAttachment = parseLargeAttachment(attachment.getDescription());
-        this.isOriginalBase64 = parseOriginalBase64(attachment.getDescription());
         this.isBase64 = Boolean.parseBoolean(attachment.getIsBase64());
         this.description = attachment.getDescription();
         this.payload = attachment.getPayload();
     }
 
-    private String parseFilename(String description) {
+    private String parseFilename(String description) throws ParseException {
         Pattern pattern = Pattern.compile("Filename=\"([A-Za-z\\d\\-_. ]*)\"");
         Matcher matcher = pattern.matcher(description);
 
@@ -36,27 +33,17 @@ public class InlineAttachment {
             return matcher.group(1);
         }
 
-        return "";
+        throw new ParseException("Unable to parse originalFilename", 0);
     }
 
-    private boolean parseCompressed(String description) {
+    private boolean parseCompressed(String description) throws ParseException {
         Pattern pattern = Pattern.compile("Compressed=(Yes|No)");
         Matcher matcher = pattern.matcher(description);
 
-        return matcher.find() && matcher.group(1).equals("Yes");
-    }
+        if (matcher.find()) {
+            return matcher.group(1).equals("Yes");
+        }
 
-    private boolean parseLargeAttachment(String description) {
-        Pattern pattern = Pattern.compile("LargeAttachment=(Yes|No)");
-        Matcher matcher = pattern.matcher(description);
-
-        return matcher.find() && matcher.group(1).equals("Yes");
-    }
-
-    private boolean parseOriginalBase64(String description) {
-        Pattern pattern = Pattern.compile("OriginalBase64=(Yes|No)");
-        Matcher matcher = pattern.matcher(description);
-
-        return matcher.find() && matcher.group(1).equals("Yes");
+        throw new ParseException("Unable to parse isCompressed", 0);
     }
 }
--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/model/InlineAttachmentTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/model/InlineAttachmentTest.java
@@ -4,9 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.text.ParseException;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import uk.nhs.adaptors.pss.translator.mhs.model.InboundMessage;
 
 public class InlineAttachmentTest {
-
-    private static final String TEXT_ATTACHMENT_PATH = "/InlineAttachments/text_attachments.txt";
     private static final String TEXT_ATTACHMENT_DESCRIPTION = """
         \t\t\t\tFilename="277F29F1-FEAB-4D38-8266-FEB7A1E6227D_LICENSE.txt"\s
         \t\t\t\tContentType=text/plain\s
@@ -33,61 +29,28 @@ public class InlineAttachmentTest {
             .contentType("text/plain")
             .isBase64("true")
             .description(TEXT_ATTACHMENT_DESCRIPTION)
-            .payload(readPayloadFromFile(TEXT_ATTACHMENT_PATH))
+            .payload("SGVsbG8gV29ybGQgZnJvbSBTY290dCBBbGV4YW5kZXI=")
             .build();
     }
 
-    private static String readPayloadFromFile(String path) {
-
-        File payloadFile = new File(path);
-        StringBuilder stringBuilder = new StringBuilder();
-
-        try (Scanner scanner = new Scanner(payloadFile)) {
-
-            while (scanner.hasNext()) {
-                stringBuilder.append(scanner.nextLine());
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return stringBuilder.toString();
-    }
-
     @Test
-    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_OriginalFileNameIsCorrect() {
+    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_OriginalFileNameIsCorrect() throws ParseException {
         InlineAttachment inlineAttachment = new InlineAttachment(textAttachment);
 
         assertEquals(TEXT_ATTACHMENT_FILENAME, inlineAttachment.getOriginalFilename());
     }
 
     @Test
-    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_CompressedIsCorrect() {
+    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_CompressedIsCorrect() throws ParseException {
         InlineAttachment inlineAttachment = new InlineAttachment(textAttachment);
 
         assertFalse(inlineAttachment.isCompressed());
     }
 
     @Test
-    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_LargeAttachmentIsCorrect() {
-        InlineAttachment inlineAttachment = new InlineAttachment(textAttachment);
-
-        assertFalse(inlineAttachment.isLargeAttachment());
-    }
-
-    @Test
-    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_OriginalBase64IsCorrect() {
-        InlineAttachment inlineAttachment = new InlineAttachment(textAttachment);
-
-        assertTrue(inlineAttachment.isOriginalBase64());
-    }
-
-    @Test
-    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_Base64IsCorrect() {
+    public void When_inlineAttachmentConstructed_withTextAttachment_Expect_Base64IsCorrect() throws ParseException {
         InlineAttachment inlineAttachment = new InlineAttachment(textAttachment);
 
         assertTrue(inlineAttachment.isBase64());
     }
-
 }
