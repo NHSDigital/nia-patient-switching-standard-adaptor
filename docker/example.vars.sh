f5c2802e5ddb7749d09854377e0bb1edcd0e7db2
scott alexander <scott.r.alexander@me.com>
2022-04-19 13:03:59 +0100

    moved yesids changes to a main based branch (#134)
    * moved yesids changes to a main based branch
    
    * missing exports readded from main

--- docker/example.vars.sh
+++ docker/example.vars.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-export PS_DB_OWNER_NAME="postgres"
-export POSTGRES_PASSWORD=
-export GPC_FACADE_USER_DB_PASSWORD=
-export GP2GP_TRANSLATOR_USER_DB_PASSWORD=
-export GP2GP_TRANSLATOR_SERVER_PORT="8085"
-export GPC_FACADE_SERVER_PORT="8081"
-export PS_QUEUE_NAME="pssQueue"
-export MHS_QUEUE_NAME="mhsQueue"
-export PS_AMQP_MAX_REDELIVERIES="3"
-export MHS_AMQP_MAX_REDELIVERIES="3"
-export MHS_BASE_URL="http://localhost:8080/"
-export PS_LOGGING_LEVEL="DEBUG"
+export DB_PORT='5436';
+export PS_DB_PORT='5436';
+export HOSTNAME='localhost';
+
+export PS_DB_URL="jdbc:postgresql://$HOSTNAME:$DB_PORT";
+export PS_DB_OWNER_NAME="postgres";
+export PS_FROM_ODS_CODE="PSS_001";
+export PS_DB_OWNER_PASSWORD=;
+export PS_DB_HOST="$HOSTNAME";
+export POSTGRES_PASSWORD=;
+export GPC_FACADE_USER_DB_PASSWORD=;
+export GP2GP_TRANSLATOR_USER_DB_PASSWORD=;
+export GP2GP_TRANSLATOR_SERVER_PORT="8085";
+export GPC_FACADE_SERVER_PORT="8081";
+export PS_QUEUE_NAME="pssQueue";
+export MHS_QUEUE_NAME="mhsQueue";
+export PS_AMQP_MAX_REDELIVERIES="3";
+export MHS_AMQP_MAX_REDELIVERIES="3";
+export MHS_BASE_URL="http://$HOSTNAME:8080/";
+export PS_LOGGING_LEVEL="DEBUG";
+
+export PS_AMQP_USERNAME="admin";
+export PS_AMQP_PASSWORD="admin";
+
+export MHS_AMQP_USERNAME="admin";
+export MHS_AMQP_PASSWORD="admin";
+
 export SSL_ENABLED=false
 export KEY_STORE=/certs/keystore.jks
 export TRUST_STORE=/certs/truststore.jks
+
+#change path for snomedFile fath
+#export SNOMED_CT_TERMINOLOGY_FILE="/mnt/c/Users/yourUserName/yourPath/uk_sct2cl_32.10.0_20220216000001Z.zip";
