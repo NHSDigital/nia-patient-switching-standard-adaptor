cd1306ba6a6c87a2f20b29396d9895283b82d684
scott alexander <scott.r.alexander@me.com>
2022-04-20 09:24:27 +0100

    export keyword added

--- docker/example.vars.sh
+++ docker/example.vars.sh
@@ -31,11 +31,11 @@ export SSL_ENABLED=false;
 export KEY_STORE=/certs/keystore.jks;
 export TRUST_STORE=/certs/truststore.jks;
 
-STORAGE_TYPE="LocalMock";
-STORAGE_REGION="";
-STORAGE_CONTAINER_NAME=""; #Local Bucket or Azure Container name
-STORAGE_REFERENCE=""; #Azure blob name or AWS user reference”
-STORAGE_SECRET=""; #Secret Key
+export STORAGE_TYPE="LocalMock";
+export STORAGE_REGION="";
+export STORAGE_CONTAINER_NAME=""; #Local Bucket or Azure Container name
+export STORAGE_REFERENCE=""; #Azure blob name or AWS user reference”
+export STORAGE_SECRET=""; #Secret Key
 
 #change path for snomedFile fath
 #export SNOMED_CT_TERMINOLOGY_FILE="/mnt/c/Users/yourUserName/yourPath/uk_sct2cl_32.10.0_20220216000001Z.zip";
