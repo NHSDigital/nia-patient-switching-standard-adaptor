fce1e988a9c199c21151957cbd6df88e2830e6bc
Ben Hession <7346923+benhession@users.noreply.github.com>
2022-04-21 18:18:29 +0100

    handle timeout for EHR Extract received migrations (#141)
    * Task 2034 (#116)
    
    * arm machine docker script added
    
    * missing files added
    
    * functional storage service for storing a single file
    
    * no functional vars removed from azure isntance
    
    * unit tests added with some improvements
    
    * ticket completion modifications completed including style checks
    
    * facade settings reset
    
    * facade settings reset 2
    
    * aws and azure pacakages added to gradle.build
    
    * another var replaced password change
    
    * another var replaced password change 2
    
    * bugspot issues fixed
    
    * checkstyle fix from spotbug fixes
    
    * code review modifications
    
    * Niad 2017 2032 yesid (#121)
    
    * NIAD-2066 use loosing ODS code to generate default ORG (#109)
    
    * NIAD-2066 use loosing ODS code to generate default ORG
    
    * NIAD-2066 typo fixed
    
    * NIAD-1944: Implement Codeable Concept Mapper using Snomed CT Database (#99)
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * spotbug final fixes
    
    * test file merge conflicts partial fix 1 broken test from merge
    
    * When_HandleMessage_WithStoreAttachmentsThrows_Expect_InlineAttachmentProcessingException error fix
    
    * comments solved
    
    Co-authored-by: dawidskainos <d.stelmaszek@kainos.com>
    Co-authored-by: bbell-kainos <87983334+bbell-kainos@users.noreply.github.com>
    Co-authored-by: scott alexander <scott.r.alexander@me.com>
    
    * spike test scheduled method.
    
    * Liquibase and jdbi spike.
    
    * Change the type of persist duration to the Java Duration type.
    
    * Initial Persist Duration logic and obtain migration requests for timeout  (#136)
    
    * Call method to update persist durations every n calls of scheduled method.
    
    * reference the migration request in the message persist table duration as a foreign key.
    
    * get the migration requests the timeout needs to be calculated for.
    
    * Call method to update persist durations every n calls of scheduled method.
    
    * reference the migration request in the message persist table duration as a foreign key.
    
    get the migration requests the timeout needs to be calculated for.
    
    * Get persist durations from SDS.
    
    * Correct error in log message.
    
    * Implement handleExtractReceivedTimeout method.
    
    * Refactor name to preserve liquibase sequence
    
    Co-authored-by: scott alexander <scott.r.alexander@me.com>
    Co-authored-by: adonaimartins <50410123+adonaimartins@users.noreply.github.com>
    Co-authored-by: dawidskainos <d.stelmaszek@kainos.com>
    Co-authored-by: bbell-kainos <87983334+bbell-kainos@users.noreply.github.com>

---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/config/SdsConfiguration.java
@@ -0,0 +1,16 @@
+package uk.nhs.adaptors.pss.translator.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "sds")
+public class SdsConfiguration {
+    private String url;
+    private String apikey;
+}
---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/config/TimeoutProperties.java
@@ -0,0 +1,16 @@
+package uk.nhs.adaptors.pss.translator.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "timeout")
+public class TimeoutProperties {
+    private int ehrExtractWeighting;
+    private int copcWeighting;
+}
