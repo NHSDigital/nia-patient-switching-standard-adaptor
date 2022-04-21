4dfd77d5f35f92cf95b2d6a1ab2902306ac0fdd9
Ben Hession <7346923+benhession@users.noreply.github.com>
2022-04-21 18:18:29 +0100

    handle timeout for EHR Extract received migrations (#141)
    * Task 2034 (#116)
    
    * arm machine docker script added
    
    * missing files added
    
    * functional storage service for storing a single file
    
    * no functional vars removed from azure isntance
    
    * unit tests added with some improvements
    
    * ticket completion modifications completed including style checks
    
    * facade settings reset
    
    * facade settings reset 2
    
    * aws and azure pacakages added to gradle.build
    
    * another var replaced password change
    
    * another var replaced password change 2
    
    * bugspot issues fixed
    
    * checkstyle fix from spotbug fixes
    
    * code review modifications
    
    * Niad 2017 2032 yesid (#121)
    
    * NIAD-2066 use loosing ODS code to generate default ORG (#109)
    
    * NIAD-2066 use loosing ODS code to generate default ORG
    
    * NIAD-2066 typo fixed
    
    * NIAD-1944: Implement Codeable Concept Mapper using Snomed CT Database (#99)
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * NIAD-2017 - NIAD-2032 continue ticket
    
    * spotbug final fixes
    
    * test file merge conflicts partial fix 1 broken test from merge
    
    * When_HandleMessage_WithStoreAttachmentsThrows_Expect_InlineAttachmentProcessingException error fix
    
    * comments solved
    
    Co-authored-by: dawidskainos <d.stelmaszek@kainos.com>
    Co-authored-by: bbell-kainos <87983334+bbell-kainos@users.noreply.github.com>
    Co-authored-by: scott alexander <scott.r.alexander@me.com>
    
    * spike test scheduled method.
    
    * Liquibase and jdbi spike.
    
    * Change the type of persist duration to the Java Duration type.
    
    * Initial Persist Duration logic and obtain migration requests for timeout  (#136)
    
    * Call method to update persist durations every n calls of scheduled method.
    
    * reference the migration request in the message persist table duration as a foreign key.
    
    * get the migration requests the timeout needs to be calculated for.
    
    * Call method to update persist durations every n calls of scheduled method.
    
    * reference the migration request in the message persist table duration as a foreign key.
    
    get the migration requests the timeout needs to be calculated for.
    
    * Get persist durations from SDS.
    
    * Correct error in log message.
    
    * Implement handleExtractReceivedTimeout method.
    
    * Refactor name to preserve liquibase sequence
    
    Co-authored-by: scott alexander <scott.r.alexander@me.com>
    Co-authored-by: adonaimartins <50410123+adonaimartins@users.noreply.github.com>
    Co-authored-by: dawidskainos <d.stelmaszek@kainos.com>
    Co-authored-by: bbell-kainos <87983334+bbell-kainos@users.noreply.github.com>

---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/exception/SdsRetrievalException.java
@@ -0,0 +1,7 @@
+package uk.nhs.adaptors.pss.translator.exception;
+
+public class SdsRetrievalException extends RuntimeException {
+    public SdsRetrievalException(String message) {
+        super(message);
+    }
+}
--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/task/scheduled/EHRTimeoutHandler.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/task/scheduled/EHRTimeoutHandler.java
@@ -2,21 +2,44 @@ package uk.nhs.adaptors.pss.translator.task.scheduled;
 
 import static uk.nhs.adaptors.connector.model.MigrationStatus.CONTINUE_REQUEST_ACCEPTED;
 import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_RECEIVED;
+import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_GENERAL_PROCESSING_ERROR;
+import static uk.nhs.adaptors.pss.translator.model.NACKReason.LARGE_MESSAGE_TIMEOUT;
+import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallString;
 
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 
+import javax.xml.bind.JAXBException;
+
+import org.hl7.v3.RCMRIN030000UK06Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import uk.nhs.adaptors.common.service.MDCService;
 import uk.nhs.adaptors.connector.model.MessagePersistDuration;
 import uk.nhs.adaptors.connector.model.PatientMigrationRequest;
 import uk.nhs.adaptors.connector.service.MessagePersistDurationService;
+import uk.nhs.adaptors.connector.service.MigrationStatusLogService;
 import uk.nhs.adaptors.connector.service.PatientMigrationRequestService;
+import uk.nhs.adaptors.pss.translator.config.TimeoutProperties;
+import uk.nhs.adaptors.pss.translator.exception.SdsRetrievalException;
+import uk.nhs.adaptors.pss.translator.mhs.model.InboundMessage;
+import uk.nhs.adaptors.pss.translator.model.NACKMessageData;
 import uk.nhs.adaptors.pss.translator.service.SDSService;
+import uk.nhs.adaptors.pss.translator.service.XPathService;
+import uk.nhs.adaptors.pss.translator.task.SendNACKMessageHandler;
+import uk.nhs.adaptors.pss.translator.util.RCRIN030000UK06Util;
 
 @Slf4j
 @Component
@@ -27,10 +50,18 @@ public class EHRTimeoutHandler {
     private static final String EHR_EXTRACT_MESSAGE_NAME = "RCMR_IN030000UK06";
     private static final String COPC_MESSAGE_NAME = "COPC_IN000001UK01";
     private static final int FREQUENCY_OF_PERSIST_DURATION_UPDATE = 3;
+    private static final String PATH_TO_TIMESTAMP = "/Envelope/Header/MessageHeader/MessageData/Timestamp";
 
     private final MessagePersistDurationService messagePersistDurationService;
     private final SDSService sdsService;
     private final PatientMigrationRequestService migrationRequestService;
+    private final MDCService mdcService;
+    private final ObjectMapper objectMapper;
+    private final XPathService xPathService;
+    private final TimeoutProperties timeoutProperties;
+    private final SendNACKMessageHandler sendNACKMessageHandler;
+    private final RCRIN030000UK06Util ehrUtil;
+    private final MigrationStatusLogService migrationStatusLogService;
 
     @Scheduled(cron = CRON_TIME)
     public void checkForTimeouts() {
@@ -44,14 +75,65 @@ public class EHRTimeoutHandler {
             migrationRequestService.getMigrationRequestByCurrentMigrationStatus(CONTINUE_REQUEST_ACCEPTED);
 
         // TODO: iterate through the migration requests:
+        //  - update the persist durations (done) and get the number of COPC messages from the DB (if the migration contains large messages)
         //  - update the persist durations and get the number of COPC messages from the DB (if the migration contains large messages)
         //  - do the timeout calculation
         //  - send the NACK message if the migration has timed out
         //  - potentially clear unnecessary data from the db after a timeout?
 
+        extractReceivedRequests.forEach(migrationRequest -> {
+            String conversationId = migrationRequest.getConversationId();
+            mdcService.applyConversationId(conversationId);
+            handleExtractReceivedTimeout(migrationRequest, conversationId);
+            mdcService.applyConversationId("");
+        });
+
+        largeMessageRequests.forEach(migrationRequest -> {
+            String conversationId = migrationRequest.getConversationId();
+            mdcService.applyConversationId(conversationId);
+            handleLargeMessageTimeout(migrationRequest, conversationId);
+            mdcService.applyConversationId("");
+        });
     }
 
-    private MessagePersistDuration getPersistDurationFor(PatientMigrationRequest migrationRequest, String messageType) {
+    private void handleExtractReceivedTimeout(PatientMigrationRequest migrationRequest, String conversationId) {
+        try {
+            Duration ehrPersistDuration = getPersistDurationFor(migrationRequest, EHR_EXTRACT_MESSAGE_NAME);
+            InboundMessage message = readMessage(migrationRequest.getInboundMessage());
+            ZonedDateTime messageTimestamp = parseMessageTimestamp(message.getEbXML());
+            ZonedDateTime currentTime = ZonedDateTime.now(messageTimestamp.getZone());
+
+            long timeout = timeoutProperties.getEhrExtractWeighting() * ehrPersistDuration.getSeconds();
+
+            if (messageTimestamp.plusSeconds(timeout).isBefore(currentTime)) {
+                sendNackMessage(message, conversationId);
+            }
+        } catch (SdsRetrievalException e) {
+            LOGGER.error("Error retrieving persist duration: [{}]", e.getMessage());
+        } catch (JsonProcessingException | SAXException | DateTimeParseException | JAXBException e) {
+            LOGGER.error("Error parsing inbound message from database");
+            migrationStatusLogService.addMigrationStatusLog(EHR_GENERAL_PROCESSING_ERROR, conversationId);
+        }
+    }
+
+    private void handleLargeMessageTimeout(PatientMigrationRequest migrationRequest, String conversationId) {
+        try {
+            Duration ehrPersistDuration = getPersistDurationFor(migrationRequest, EHR_EXTRACT_MESSAGE_NAME);
+            Duration copcPersistDuration = getPersistDurationFor(migrationRequest, COPC_MESSAGE_NAME);
+
+            InboundMessage message = readMessage(migrationRequest.getInboundMessage());
+
+            ZonedDateTime messageTimestamp = parseMessageTimestamp(message.getEbXML());
+
+        } catch (SdsRetrievalException e) {
+            LOGGER.error("Error retrieving persist duration: [{}]", e.getMessage());
+        } catch (JsonProcessingException | SAXException | DateTimeParseException e) {
+            LOGGER.error("Error parsing inbound message from database");
+            migrationStatusLogService.addMigrationStatusLog(EHR_GENERAL_PROCESSING_ERROR, conversationId);
+        }
+    }
+
+    private Duration getPersistDurationFor(PatientMigrationRequest migrationRequest, String messageType) {
 
         Optional<MessagePersistDuration> messageDurationOptional =
             messagePersistDurationService.getMessagePersistDuration(migrationRequest.getId(), messageType);
@@ -61,10 +143,12 @@ public class EHRTimeoutHandler {
 
             return messagePersistDurationService.addMessagePersistDuration(
                 messageType,
-                sdsService.getPersistDurationFor(messageType, migrationRequest.getLosingPracticeOdsCode()),
+
+                sdsService.getPersistDurationFor(messageType, migrationRequest.getLosingPracticeOdsCode(),
+                    migrationRequest.getConversationId()),
                 1,
                 migrationRequest.getId()
-            );
+            ).getPersistDuration();
         }
 
         return messageDurationOptional.map(mpd ->
@@ -73,7 +157,36 @@ public class EHRTimeoutHandler {
                 mpd.getPersistDuration(),
                 mpd.getCallsSinceUpdate() + 1,
                 migrationRequest.getId()
-            )
+            ).getPersistDuration()
         ).orElseThrow();
+    }
+
+    private InboundMessage readMessage(String message) throws JsonProcessingException {
+        return objectMapper.readValue(message, InboundMessage.class);
+    }
+
+    private ZonedDateTime parseMessageTimestamp(String ebXML) throws SAXException, DateTimeParseException {
+        Document document = xPathService.parseDocumentFromXml(ebXML);
+        return ZonedDateTime.parse(xPathService.getNodeValue(document, PATH_TO_TIMESTAMP));
+    }
+
+    private void sendNackMessage(InboundMessage inboundMessage, String conversationId) throws JAXBException {
+
+        RCMRIN030000UK06Message payload = unmarshallString(inboundMessage.getPayload(), RCMRIN030000UK06Message.class);
+
+        NACKMessageData messageData = NACKMessageData
+            .builder()
+            .nackCode(LARGE_MESSAGE_TIMEOUT.getCode())
+            .fromAsid(ehrUtil.parseFromAsid(payload))
+            .toAsid(ehrUtil.parseToAsid(payload))
+            .toOdsCode(ehrUtil.parseToOdsCode(payload))
+            .messageRef(ehrUtil.parseMessageRef(payload))
+            .conversationId(conversationId)
+            .build();
+
+        LOGGER.debug("EHR Extract message timed out: sending NACK message");
+        migrationStatusLogService.addMigrationStatusLog(LARGE_MESSAGE_TIMEOUT.getMigrationStatus(), conversationId);
+
+        sendNACKMessageHandler.prepareAndSendMessage(messageData);
     }
 }
