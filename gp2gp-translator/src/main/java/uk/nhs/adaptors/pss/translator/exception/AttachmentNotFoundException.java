d68b41c2221cb6061d90e8cf00ad94b61937f424
ellenwootten <ellen.wootten@madetech.com>
2022-04-26 14:06:15 +0100

    Fix AttachmentNotFoundException spotbug issue

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/exception/AttachmentNotFoundException.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/exception/AttachmentNotFoundException.java
@@ -1,6 +1,6 @@
 package uk.nhs.adaptors.pss.translator.exception;
 
-public class AttachmentNotFoundException extends Throwable {
+public class AttachmentNotFoundException extends Exception {
     public AttachmentNotFoundException(String message) {
         super(message);
     }
