a2e72419c9c158350bc2dfbff0832aeee40bfc57
mrogowska <m.rogowska@kainos.com>
2022-05-31 16:14:26 +0200

    NIAD-2193: Try change activemq dockerfile

--- docker/vars.local.tests.sh
+++ docker/vars.local.tests.sh
@@ -16,12 +16,3 @@ export PS_AMQP_BROKER="amqp://activemq:5672"
 export MHS_AMQP_BROKER="amqp://activemq:5672"
 export MHS_BASE_URL="http://mhs-adaptor-mock:8080/"
 export PS_LOGGING_LEVEL="DEBUG"
-
-export STORAGE_TYPE="LocalMock";
-export STORAGE_REGION="";
-export STORAGE_CONTAINER_NAME=""; #Local Bucket or Azure Container name
-export STORAGE_REFERENCE=""; #Azure blob name or AWS user reference”
-export STORAGE_SECRET=""; #Secret Key
-
-export SDS_API_key=""
-export SDS_BASE_URL="https://int.api.service.nhs.uk/spine-directory/FHIR/R4/"
