25a38d05d09950efe07cad4beeea0066f3f863ee
Yesid Martins <andreg325@hotmail.com>
2022-06-29 15:36:59 +0100

    NIAD-2177 bug fixed and integration test fixed

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationStatementMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationStatementMapper.java
@@ -45,7 +45,7 @@ public class MedicationStatementMapper {
     private static final String MS_LAST_ISSUE_DATE = "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC"
         + "-MedicationStatementLastIssueDate-1";
     private static final String PRESCRIBING_AGENCY_URL
-        = "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-PrescribingAgency-1";
+        = "https://fhir.nhs.uk/STU3/CodeSystem/CareConnect-PrescribingAgency-1";
 
     private static final String MS_SUFFIX = "-MS";
     private static final String PRESCRIBED_CODE = "prescribed-at-gp-practice";
