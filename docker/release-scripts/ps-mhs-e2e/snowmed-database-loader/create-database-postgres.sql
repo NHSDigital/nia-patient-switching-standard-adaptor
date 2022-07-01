eac360b52a139f2f865eb36f8644f7ba60cdd063
scott alexander <scott.r.alexander@me.com>
2022-07-01 10:51:46 +0100

    release scripts first parse and working in isolation

---
+++ docker/release-scripts/ps-mhs-e2e/snowmed-database-loader/create-database-postgres.sql
@@ -0,0 +1,40 @@
+drop schema if exists snomedct cascade;
+create schema snomedct;
+set schema 'snomedct';
+
+create table description_s(
+                              id varchar(18) not null,
+                              effectivetime char(8) not null,
+                              active char(1) not null,
+                              moduleid varchar(18) not null,
+                              conceptid varchar(18) not null,
+                              languagecode varchar(2) not null,
+                              typeid varchar(18) not null,
+                              term text not null,
+                              casesignificanceid varchar(18) not null,
+                              PRIMARY KEY(id, effectivetime)
+);
+CREATE INDEX description_conceptid_idx ON snomedct.description_s
+    USING btree (conceptid COLLATE pg_catalog."default");
+
+create table langrefset_s(
+                             id uuid not null,
+                             effectivetime char(8) not null,
+                             active char(1) not null,
+                             moduleid varchar(18) not null,
+                             refsetid varchar(18) not null,
+                             referencedcomponentid varchar(18) not null,
+                             acceptabilityid varchar(18) not null,
+                             PRIMARY KEY(id, effectivetime)
+);
+CREATE INDEX langrefset_referencedcomponentid_idx ON snomedct.langrefset_s
+    USING btree (referencedcomponentid);
+
+CREATE TABLE immunization_codes(
+                              conceptid varchar(18) not null,
+                              description text not null,
+                              safetycode varchar(18) not null
+);
+
+GRANT USAGE ON SCHEMA snomedct TO application_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA snomedct TO application_user;
