9ec7102e7f3732c1397331855e55e927e9cbc8b1
Scott Alexander <scott.alexander@madetech.com>
2022-10-17 12:18:17 +0100

    reverted non required changes

--- docker/start-local-environment-arm.sh
+++ docker/start-local-environment-arm.sh
@@ -6,12 +6,13 @@ source vars.sh
 if [[ "$(docker network ls | grep "ps-network")" == "" ]] ; then
     docker network create ps-network
 fi
-#docker-compose -f docker-compose-arm.yml build ps_db mhs-adaptor-mock activemq;
-#docker-compose -f docker-compose-arm.yml up -d ps_db mhs-adaptor-mock activemq;
-#
-#cd ../db-connector
-#./gradlew update
-#cd ../docker
+
+docker-compose -f docker-compose-arm.yml build ps_db mhs-adaptor-mock activemq;
+docker-compose -f docker-compose-arm.yml up -d ps_db mhs-adaptor-mock activemq;
+
+cd ../db-connector
+./gradlew update
+cd ../docker
 
 docker-compose -f docker-compose-arm.yml build gpc_facade gp2gp_translator;
 docker-compose -f docker-compose-arm.yml up gpc_facade gp2gp_translator;
