7d4b5cd2f246e318d25233e1f181aa9a11341233
Martin Wheeler <martin.wheeler@madetech.com>
2022-11-15 07:50:35 +0000

    NIAD-2299 Add AcknowledgementController Add associated unit tests

--- gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/controller/AcknowledgeRecordController.java
+++ gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/controller/AcknowledgeRecordController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.nhs.adaptors.pss.gpc.service.AcknowledgeRecordService;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.Map;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
+import static uk.nhs.adaptors.pss.gpc.controller.handler.FhirMediaTypes.APPLICATION_FHIR_JSON_VALUE;
 import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.CONFIRMATION_RESPONSE;
 import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.CONVERSATION_ID;
 
@@ -25,19 +26,16 @@ public class AcknowledgeRecordController {
     private final AcknowledgeRecordService acknowledgeRecordService;
 
     @PostMapping(
-            path = "/$gpc.ack"
+            path = "/$gpc.ack",
+            produces = {APPLICATION_FHIR_JSON_VALUE}
     )
     public ResponseEntity<String> acknowledgeRecord(
-            @RequestHeader(CONFIRMATION_RESPONSE) @NotNull String confirmationResponse,
-            @RequestHeader(CONVERSATION_ID) @NotNull String conversationId
+            @RequestHeader(CONFIRMATION_RESPONSE) @NotNull @NotEmpty String confirmationResponse,
+            @RequestHeader(CONVERSATION_ID) @NotNull @NotEmpty String conversationId
             ) {
         LOGGER.info("Received migrated structured record acknowledgement");
-        Map<String, String> headers = Map.of(
-                CONFIRMATION_RESPONSE, confirmationResponse,
-                CONVERSATION_ID, conversationId
-        );
 
-        var response = acknowledgeRecordService.handleAcknowledgeRecord(headers);
+        var response = acknowledgeRecordService.handleAcknowledgeRecord(conversationId, confirmationResponse);
 
         return response
                 ? new ResponseEntity<>(OK)
--- gpc-api-facade/src/test/java/uk/nhs/adaptors/pss/gpc/controller/AcknowledgeRecordControllerTest.java
+++ gpc-api-facade/src/test/java/uk/nhs/adaptors/pss/gpc/controller/AcknowledgeRecordControllerTest.java
@@ -1,10 +1,5 @@
 package uk.nhs.adaptors.pss.gpc.controller;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.CONFIRMATION_RESPONSE;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.CONVERSATION_ID;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,18 +9,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.nhs.adaptors.pss.gpc.service.AcknowledgeRecordService;
 
-import java.util.Map;
 import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AcknowledgeRecordControllerTest {
     private static final String CONVERSATION_ID_VALUE = UUID.randomUUID().toString();
     private static final String CONFIRMATION_RESPONSE_VALUE = "accepted";
-
-    private static final Map<String, String> HEADERS = Map.of(
-            CONVERSATION_ID, CONVERSATION_ID_VALUE,
-            CONFIRMATION_RESPONSE, CONFIRMATION_RESPONSE_VALUE
-    );
 
 
     @Mock
@@ -37,12 +29,12 @@ public class AcknowledgeRecordControllerTest {
 
     @Test
     public void acknowledgeMigratedStructuredRecordWhenServiceReturnsFalseShouldReturn500() {
-        when(acknowledgeRecordService.handleAcknowledgeRecord(HEADERS))
+        when(acknowledgeRecordService.handleAcknowledgeRecord(CONFIRMATION_RESPONSE_VALUE, CONVERSATION_ID_VALUE))
                 .thenReturn(false);
 
         ResponseEntity<String> response = controller.acknowledgeRecord(
-                CONFIRMATION_RESPONSE_VALUE,
-                CONVERSATION_ID_VALUE);
+                CONVERSATION_ID_VALUE,
+                CONFIRMATION_RESPONSE_VALUE);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
         assertThat(response.getBody()).isNull();
@@ -50,7 +42,7 @@ public class AcknowledgeRecordControllerTest {
 
     @Test
     public void acknowledgeMigratedStructuredRecordWhenServiceReturnsTrueShouldReturn200() {
-        when(acknowledgeRecordService.handleAcknowledgeRecord(HEADERS))
+        when(acknowledgeRecordService.handleAcknowledgeRecord(CONVERSATION_ID_VALUE, CONFIRMATION_RESPONSE_VALUE))
                 .thenReturn(true);
 
         ResponseEntity<String> response = controller.acknowledgeRecord(
