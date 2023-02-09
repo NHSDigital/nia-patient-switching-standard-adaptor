ebb759d5d2b702a3f7ab6ba9654951df1cbfee6b
BenHession <bshession@gmail.com>
2023-02-09 18:39:28 +0000

    Add util method to extract HL7 PIMP comments.

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/TextUtil.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/TextUtil.java
@@ -1,8 +1,14 @@
 package uk.nhs.adaptors.pss.translator.util;
 
+import java.util.regex.Pattern;
+
 import org.apache.commons.lang3.StringUtils;
+import org.intellij.lang.annotations.RegExp;
 
 public class TextUtil {
+
+    @RegExp
+    private static final String PIMP_COMMENT_REGEX = "^CommentType:[A-Za-z0-9() ]+[\\s\\n]+CommentDate:[0-9]+\\s*\\n+([\\S\\s]*)$";
 
     public static String getLastLine(String text) {
         if (text == null) {
@@ -19,5 +25,16 @@ public class TextUtil {
 
     public static String addLine(String text, String line) {
         return text == null ? line : text.concat(StringUtils.LF).concat(line);
+    }
+
+    public static String extractPimpComment(String text) {
+        var pattern = Pattern.compile(PIMP_COMMENT_REGEX);
+        var matcher = pattern.matcher(text);
+
+        if (!matcher.find()) {
+            return text;
+        }
+
+        return matcher.group(1);
     }
 }
--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/util/TextUtilTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/util/TextUtilTest.java
@@ -3,8 +3,12 @@ package uk.nhs.adaptors.pss.translator.util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import static uk.nhs.adaptors.pss.translator.util.TextUtil.addLine;
+import static uk.nhs.adaptors.pss.translator.util.TextUtil.extractPimpComment;
 import static uk.nhs.adaptors.pss.translator.util.TextUtil.getLastLine;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,8 +16,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import lombok.SneakyThrows;
 
 public class TextUtilTest {
+
+    private static final String PIMP_COMMENTS_DIR = "pimp-comments/";
 
     private static final String THREE_LINER = """
         HELLO
@@ -46,6 +56,45 @@ public class TextUtilTest {
         assertThat(shouldBeFourLiner.split(StringUtils.LF).length).isEqualTo(4);
     }
 
+    @ParameterizedTest
+    @MethodSource("pimpComments")
+    @SneakyThrows
+    public void When_ExtractPimpComment_WithValidComment_Expect_CommentExtracted(String inputFile, String outputFile) {
+        var input = readFileAsString(PIMP_COMMENTS_DIR + inputFile);
+        var expectedOutput = readFileAsString(PIMP_COMMENTS_DIR + outputFile);
+
+        var output = extractPimpComment(input);
+
+        assertThat(output).isEqualTo(expectedOutput);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidPimpComments")
+    @SneakyThrows
+    public void When_ExtractPimpComment_WithInvalidComment_Expect_InputReturned(String inputFile) {
+        var input = readFileAsString(PIMP_COMMENTS_DIR + inputFile);
+
+        var output = extractPimpComment(input);
+
+        assertThat(output).isEqualTo(input);
+    }
+
+    private static Stream<Arguments> pimpComments() {
+        return Stream.of(
+            Arguments.of("aggregate-comment-1.txt", "aggregate-comment-1-output.txt"),
+            Arguments.of("aggregate-comment-2.txt", "aggregate-comment-2-output.txt"),
+            Arguments.of("lab-specimen-comment-1.txt", "lab-specimen-comment-1-output.txt"),
+            Arguments.of("laboratory-result-comment-1.txt", "laboratory-result-comment-1-output.txt"),
+            Arguments.of("laboratory-result-comment-2.txt", "laboratory-result-comment-2-output.txt"),
+            Arguments.of("user-comment-1.txt", "user-comment-1-output.txt")
+            );
+    }
+
+    private static Stream<Arguments> invalidPimpComments() {
+        return Stream.of(
+            Arguments.of("invalid-pimp-comment-1.txt"));
+    }
+
     private static Stream<Arguments> testInputs() {
         return Stream.of(
             Arguments.of(ONE_LINER, ONE_LINER),
@@ -57,4 +106,8 @@ public class TextUtilTest {
         );
     }
 
+    private String readFileAsString(String path) throws IOException {
+        Resource resource = new ClassPathResource(path);
+        return Files.readString(Paths.get(resource.getURI()));
+    }
 }
