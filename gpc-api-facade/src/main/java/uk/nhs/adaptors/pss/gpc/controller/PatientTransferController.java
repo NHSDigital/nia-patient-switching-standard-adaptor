18219d4bf9e69f0280a4034b4ecd36e06b69ae78
Martin Wheeler <martin.wheeler@madetech.com>
2023-03-29 17:00:39 +0100

    NIAD-2614 Update PatientTransferService to retrieve conversationId from existing incomplete request Update patientTransferController to handle already existing patient transfer request

--- gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/controller/PatientTransferController.java
+++ gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/controller/PatientTransferController.java
@@ -1,50 +1,7 @@
 package uk.nhs.adaptors.pss.gpc.controller;
 
-import static org.hl7.fhir.dstu3.model.OperationOutcome.IssueSeverity.ERROR;
-import static org.hl7.fhir.dstu3.model.OperationOutcome.IssueType.EXCEPTION;
-import static org.springframework.http.HttpStatus.ACCEPTED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
-import static org.springframework.http.HttpStatus.OK;
-
-import static uk.nhs.adaptors.connector.model.MigrationStatus.CONTINUE_REQUEST_ACCEPTED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.COPC_ACKNOWLEDGED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.COPC_FAILED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.COPC_MESSAGE_PROCESSING;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.COPC_MESSAGE_RECEIVED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_PROCESSING;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_RECEIVED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_ACCEPTED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_ACKNOWLEDGED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_EHR_GENERATION_ERROR;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_MISFORMED_REQUEST;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_MULTI_OR_NO_RESPONSES;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_NOT_PRIMARY_HEALTHCARE_PROVIDER;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_PATIENT_NOT_REGISTERED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_SENDER_NOT_CONFIGURED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK_UNKNOWN;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_EXTRACT_TRANSLATED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.EHR_GENERAL_PROCESSING_ERROR;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.ERROR_LRG_MSG_ATTACHMENTS_NOT_RECEIVED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.ERROR_LRG_MSG_GENERAL_FAILURE;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.ERROR_LRG_MSG_REASSEMBLY_FAILURE;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.ERROR_LRG_MSG_TIMEOUT;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.FINAL_ACK_SENT;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.MIGRATION_COMPLETED;
-import static uk.nhs.adaptors.connector.model.MigrationStatus.REQUEST_RECEIVED;
-import static uk.nhs.adaptors.pss.gpc.controller.handler.FhirMediaTypes.APPLICATION_FHIR_JSON_VALUE;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.FROM_ASID;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.FROM_ODS;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.TO_ASID;
-import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.TO_ODS;
-import static uk.nhs.adaptors.pss.gpc.util.fhir.OperationOutcomeUtils.createOperationOutcome;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-import javax.validation.constraints.NotBlank;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
 import org.hl7.fhir.dstu3.model.Parameters;
@@ -56,15 +13,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import uk.nhs.adaptors.common.enums.MigrationStatus;
 import uk.nhs.adaptors.common.util.CodeableConceptUtils;
 import uk.nhs.adaptors.common.util.fhir.FhirParser;
-import uk.nhs.adaptors.connector.model.MigrationStatus;
 import uk.nhs.adaptors.connector.model.MigrationStatusLog;
 import uk.nhs.adaptors.pss.gpc.controller.validation.PatientTransferRequest;
 import uk.nhs.adaptors.pss.gpc.service.PatientTransferService;
+
+import javax.validation.constraints.NotBlank;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.hl7.fhir.dstu3.model.OperationOutcome.IssueSeverity.ERROR;
+import static org.hl7.fhir.dstu3.model.OperationOutcome.IssueType.EXCEPTION;
+import static org.springframework.http.HttpStatus.ACCEPTED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.nhs.adaptors.common.enums.MigrationStatus.EHR_EXTRACT_REQUEST_NEGATIVE_ACK;
+import static uk.nhs.adaptors.common.enums.MigrationStatus.EHR_GENERAL_PROCESSING_ERROR;
+import static uk.nhs.adaptors.common.enums.MigrationStatus.FINAL_ACK_SENT;
+import static uk.nhs.adaptors.common.enums.MigrationStatus.MIGRATION_COMPLETED;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.GPG2PG_NACK_400_ERROR_STATUSES;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.GPG2PG_NACK_404_ERROR_STATUSES;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.GPG2PG_NACK_500_ERROR_STATUSES;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.GPG2PG_NACK_501_ERROR_STATUSES;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.IN_PROGRESS_STATUSES;
+import static uk.nhs.adaptors.common.model.MigrationStatusGroups.LRG_MESSAGE_ERRORS;
+import static uk.nhs.adaptors.pss.gpc.controller.handler.FhirMediaTypes.APPLICATION_FHIR_JSON_VALUE;
+import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.FROM_ASID;
+import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.FROM_ODS;
+import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.TO_ASID;
+import static uk.nhs.adaptors.pss.gpc.controller.header.HttpHeaders.TO_ODS;
+import static uk.nhs.adaptors.pss.gpc.util.fhir.OperationOutcomeUtils.createOperationOutcome;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -73,47 +53,6 @@ import uk.nhs.adaptors.pss.gpc.service.PatientTransferService;
 public class PatientTransferController {
 
     private static final String ISSUE_SYSTEM = "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1";
-
-    private static final List<MigrationStatus> IN_PROGRESS_STATUSES = List.of(
-        REQUEST_RECEIVED,
-        EHR_EXTRACT_REQUEST_ACCEPTED,
-        EHR_EXTRACT_RECEIVED,
-        EHR_EXTRACT_PROCESSING,
-        EHR_EXTRACT_REQUEST_ACKNOWLEDGED,
-        EHR_EXTRACT_TRANSLATED,
-        CONTINUE_REQUEST_ACCEPTED,
-        COPC_MESSAGE_RECEIVED,
-        COPC_MESSAGE_PROCESSING,
-        COPC_ACKNOWLEDGED,
-        COPC_FAILED
-    );
-
-    private static final List<MigrationStatus> GPG2PG_NACK_400_ERROR_STATUSES = List.of(
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_MISFORMED_REQUEST
-    );
-
-    private static final List<MigrationStatus> GPG2PG_NACK_404_ERROR_STATUSES = List.of(
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_PATIENT_NOT_REGISTERED,
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_NOT_PRIMARY_HEALTHCARE_PROVIDER
-    );
-
-    private static final List<MigrationStatus> GPG2PG_NACK_500_ERROR_STATUSES = List.of(
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_EHR_GENERATION_ERROR,
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_MULTI_OR_NO_RESPONSES,
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_UNKNOWN
-    );
-
-    private static final List<MigrationStatus> GPG2PG_NACK_501_ERROR_STATUSES = List.of(
-        EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_SENDER_NOT_CONFIGURED
-    );
-
-    private static final List<MigrationStatus> LRG_MESSAGE_ERRORS = List.of(
-        ERROR_LRG_MSG_REASSEMBLY_FAILURE,
-        ERROR_LRG_MSG_ATTACHMENTS_NOT_RECEIVED,
-        ERROR_LRG_MSG_GENERAL_FAILURE,
-        ERROR_LRG_MSG_TIMEOUT
-    );
-
     private final PatientTransferService patientTransferService;
     private final FhirParser fhirParser;
 
@@ -135,6 +74,13 @@ public class PatientTransferController {
             TO_ODS, toOds,
             FROM_ODS, fromOds
         );
+
+        var existingConversationId = patientTransferService.checkExistingPatientMigrationRequestInProgress(body);
+        if (existingConversationId != null) {
+            var operationOutcome = createErrorBodyForInProgressRequest(existingConversationId);
+            var errorBody =  fhirParser.encodeToJson(operationOutcome);
+            return new ResponseEntity<>(errorBody, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
         MigrationStatusLog request = patientTransferService.handlePatientMigrationRequest(body, headers);
         if (request == null) {
@@ -175,10 +121,19 @@ public class PatientTransferController {
         }
     }
 
+    private OperationOutcome createErrorBodyForInProgressRequest(String conversationId) {
+        var operationErrorCode = "INTERNAL_SERVER_ERROR";
+        var operationErrorMessage = "PS - The Given NHS number is already being processed against Conversation ID: "
+            + conversationId + "you cannot start a new request until the current request has completed or failed.";
+
+        CodeableConcept details = CodeableConceptUtils.
+                createCodeableConcept(operationErrorCode, ISSUE_SYSTEM, operationErrorMessage, null);
+        return createOperationOutcome(EXCEPTION, ERROR, details, "");
+    }
     private OperationOutcome createErrorBodyFromMigrationStatus(MigrationStatus migrationStatus) throws IOException {
 
-        String operationErrorCode = "";
-        String operationErrorMessage = "";
+        String operationErrorCode;
+        String operationErrorMessage;
 
         switch (migrationStatus) {
             case EHR_EXTRACT_REQUEST_NEGATIVE_ACK_GP2GP_MISFORMED_REQUEST:
@@ -235,8 +190,7 @@ public class PatientTransferController {
 
         CodeableConcept details = CodeableConceptUtils.
                 createCodeableConcept(operationErrorCode, ISSUE_SYSTEM, operationErrorMessage, null);
-        OperationOutcome operationOutcome = createOperationOutcome(EXCEPTION, ERROR, details, "");
-        return operationOutcome;
+        return createOperationOutcome(EXCEPTION, ERROR, details, "");
     }
 
 
