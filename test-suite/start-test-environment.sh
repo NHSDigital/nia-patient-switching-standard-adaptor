00649655ceba775ed7f50c03c7e78b942f881a37
Ben Hession <7346923+benhession@users.noreply.github.com>
2023-07-06 11:28:08 +0100

    Update test suite container versions and tidy up scripts. (#290)

--- test-suite/start-test-environment.sh
+++ test-suite/start-test-environment.sh
@@ -14,28 +14,23 @@ chmod +x gradlew
 
 cd ..
 
-
-LIGHT_GREEN='\033[1;32m'
-RED='\033[0;31m'
-NC='\033[0m'
-
-echo "${LIGHT_GREEN}Exporting environment variables${NC}"
+echo "Exporting environment variables"
 
 if [[ -z "${MHS_SECRET_PARTY_KEY}" ]]; then
-  echo "${RED}Secret key not set for MHS_SECRET_PARTY_KEY${NC}"
+  echo "Secret key not set for MHS_SECRET_PARTY_KEY"
   exit 1
 elif [[ -z "${MHS_SECRET_CLIENT_CERT}" ]]; then
-  echo "${RED}Secret key not set for MHS_SECRET_CLIENT_CERT${NC}"
+  echo "Secret key not set for MHS_SECRET_CLIENT_CERT"
   exit 1
 elif [[ -z "${MHS_SECRET_CLIENT_KEY}" ]]; then
-  echo "${RED}Secret key not set for MHS_SECRET_CLIENT_KEY${NC}"
+  echo "Secret key not set for MHS_SECRET_CLIENT_KEY"
   exit 1
 elif [[ -z "${MHS_SECRET_CA_CERTS}" ]]; then
-  echo "${RED}Secret key not set for MHS_SECRET_CA_CERTS${NC}"
+  echo "Secret key not set for MHS_SECRET_CA_CERTS"
   exit 1
 fi
 
-echo "${LIGHT_GREEN}Running containers${NC}"
+echo "Running containers"
 
 if [[ "$(docker network ls | grep "nia-ps")" == "" ]] ; then
     docker network create nia-ps
