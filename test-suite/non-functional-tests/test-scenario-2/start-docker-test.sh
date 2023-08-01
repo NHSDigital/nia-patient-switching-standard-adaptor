f07720c449ab111744cc5a9c285bb051236edf15
Martin Wheeler <martin.wheeler@madetech.com>
2023-08-01 15:46:57 +0100

    NIAD-2455 update docker files and scripts for best practice

--- test-suite/non-functional-tests/test-scenario-2/start-docker-test.sh
+++ test-suite/non-functional-tests/test-scenario-2/start-docker-test.sh
@@ -2,10 +2,8 @@
 
 openssl pkcs12 -export -out jmeterkeystore.p12 -inkey ../certs/client.key -in ../certs/client.crt -passout pass:password
 
-echo Stopping docker container:
-docker ps -a -q -f name=nf-test-2 | xargs -r docker stop
-echo Removing Docker container:
-docker ps -a -q -f name=nf-test-2 | xargs -r docker rm
+echo Removing Docker container if present...
+docker compose rm -s -f name=nf-test-2
 
 docker compose build nf-test-2 
 docker compose up nf-test-2
