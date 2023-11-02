9e1d8fa9e1f60159551349db7a16bef12680c556
MartinWheelerMT <martin.wheeler@madetech.com>
2023-11-02 16:52:21 +0000

    refactor create-database-postgres.sql to make the materialised view easier to understand

--- snomed-database-loader/create-database-postgres.sql
+++ snomed-database-loader/create-database-postgres.sql
@@ -51,36 +51,36 @@ CREATE INDEX relationship_typeId_idx ON snomedct.relationship_s
     USING btree (typeId);
 
 CREATE MATERIALIZED VIEW immunization_codes AS
-WITH RECURSIVE relationship_cte AS (
-    -- Start with the root conceptIds for immunizations where relationshipType is 'IsA' (child of)
-    SELECT DISTINCT sourceId
+WITH RECURSIVE immunisation_heirarchy AS (
+    -- Start with the root conceptIds for immunizations
+    SELECT DISTINCT sourceId AS conceptId
     FROM snomedct.relationship_s
-    WHERE typeId = '116680003'
+    WHERE typeId = '116680003' -- relationshipType (typeId) = 'IsA' (meaning child of)
     AND destinationId IN ('787859002','127785005','304250009','90351000119108','713404003', '127785005')
     UNION
-    -- add known other values not covered by root codes and add the actual root codes themselves
-    SELECT conceptId
-    FROM (VALUES
-              ('787859002'),('127785005'),('304250009'),('90351000119108'),('713404003'), ('127785005'),
-              ('868241000000109'),('572421000119102'),('626071000119103'),('16298561000119108'),
-              ('571611000119101'),('170415002'),('170408005'),('412743000'),('7141001'),('270898008'),
-              ('170399005'),('170412004'),('170405008'),('170400003'),('170395004'),('313188000'),
-              ('51572002'),('8605003'),('39568006'),('275849001'),('390892002'),('281040003'),('275841003'),
-              ('777611000000100'),('1240491000000103'),('170413009'),('170406009'),('170401004'),
-              ('170396003'),('313189008'),('308753004'),('170409002'),('170414003'),('170407000'),
-              ('170402006'),('170397007'),('412742005'),('170425007'),('170427004'),('170426008'),
-              ('312867004'),('570001'),('787438002'),('428570002')
-         ) AS additional_conceptIds(conceptId)
-    UNION
-    -- Recursively find child records where relationshipType is 'IsA' (child of)
+    -- Recursively find child records
     -- if no records are found then the maximum depth for this recursion has been reached
     SELECT r.sourceId
     FROM snomedct.relationship_s r
-             JOIN relationship_cte d ON r.destinationId = d.sourceId
-    WHERE r.typeId = '116680003'
+             JOIN immunisation_heirarchy i ON r.destinationId = i.conceptId
+    WHERE r.typeId = '116680003' -- relationshipType (typeId) is 'IsA' (child of)
 )
-SELECT sourceid AS conceptId
-FROM relationship_cte;
+SELECT conceptId
+FROM immunisation_heirarchy
+UNION
+-- add known other values not covered by root codes and add the actual root codes themselves
+SELECT conceptId
+FROM (VALUES
+          ('787859002'),('127785005'),('304250009'),('90351000119108'),('713404003'), ('127785005'),
+          ('868241000000109'),('572421000119102'),('626071000119103'),('16298561000119108'),
+          ('571611000119101'),('170415002'),('170408005'),('412743000'),('7141001'),('270898008'),
+          ('170399005'),('170412004'),('170405008'),('170400003'),('170395004'),('313188000'),
+          ('51572002'),('8605003'),('39568006'),('275849001'),('390892002'),('281040003'),('275841003'),
+          ('777611000000100'),('1240491000000103'),('170413009'),('170406009'),('170401004'),
+          ('170396003'),('313189008'),('308753004'),('170409002'),('170414003'),('170407000'),
+          ('170402006'),('170397007'),('412742005'),('170425007'),('170427004'),('170426008'),
+          ('312867004'),('570001'),('787438002'),('428570002')
+     ) AS conceptIds_not_in_immunisation_heirachy(conceptId);
 
 GRANT USAGE ON SCHEMA snomedct TO application_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA snomedct TO application_user;
