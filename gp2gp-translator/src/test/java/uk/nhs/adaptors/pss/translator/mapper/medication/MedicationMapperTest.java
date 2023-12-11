30e1dc9592bd64beb2446fcf38d790bbecd28e8c
MartinWheelerMT <martin.wheeler@madetech.com>
2023-12-11 09:40:56 +0000

    Refactor MedicationMapperTest tp inline XML for tests Remove now unneeded xml filmes

--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationMapperTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationMapperTest.java
@@ -2,11 +2,11 @@ package uk.nhs.adaptors.pss.translator.mapper.medication;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
-import static org.springframework.util.ResourceUtils.getFile;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
-import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallFile;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallString;
 
 import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.v3.RCMRMT030101UK04Consumable;
@@ -24,10 +24,6 @@ import uk.nhs.adaptors.pss.translator.util.DegradedCodeableConcepts;
 
 @ExtendWith(MockitoExtension.class)
 public class MedicationMapperTest {
-
-    private static final String XML_RESOURCES_BASE = "xml/Consumable/";
-    private static final String TEST_ID = "TEST_ID";
-
     @Mock
     private CodeableConceptMapper codeableConceptMapper;
 
@@ -47,7 +43,24 @@ public class MedicationMapperTest {
 
     @Test
     public void When_MappingConsumable_Expect_CorrectFieldsMapped() {
-        var consumable = unmarshallConsumable("consumable1.xml");
+
+        var inputXml = """
+                <consumable xmlns="urn:hl7-org:v3" typeCode="CSM">
+                    <manufacturedProduct classCode="MANU">
+                        <manufacturedMaterial classCode="MMAT" determinerCode="KIND">
+                            <code codeSystem="2.16.840.1.113883.2.1.6.15" code="58976020"
+                            displayName="Bendroflumethiazide 2.5mg tablets">
+                                <originalText>BENDROFLUMETHIAZIDE tabs 2.5mg</originalText>
+                                <translation code="317919004" displayName="Bendroflumethiazide 2.5mg tablets"
+                                codeSystem="2.16.840.1.113883.2.1.3.2.4.15"/>
+                                <translation code="b211.00" codeSystem="2.16.840.1.113883.2.1.6.2"/>
+                            </code>
+                        </manufacturedMaterial>
+                    </manufacturedProduct>
+                </consumable>
+                """;
+
+        var consumable = unmarshallConsumable(inputXml);
         var medication = medicationMapper.createMedication(consumable);
 
         verify(codeableConceptMapper, times(1)).mapToCodeableConceptForMedication(any());
@@ -58,7 +71,19 @@ public class MedicationMapperTest {
 
     @Test
     public void When_MappingConsumableWithCodeNotFound_Expect_CorrectFieldsMappedWithDegraded() {
-        var consumable = unmarshallConsumable("degraded_consumable.xml");
+        var inputXml = """
+                <consumable xmlns="urn:hl7-org:v3" typeCode="CSM">
+                    <manufacturedProduct classCode="MANU">
+                        <manufacturedMaterial classCode="MMAT" determinerCode="KIND">
+                            <code code="05652001"
+                            codeSystem="2.16.840.1.113883.2.1.6.4" 
+                            displayName="AGAROL liquid [WARN/LAMB]"/>
+                        </manufacturedMaterial>
+                    </manufacturedProduct>
+                </consumable>
+                """;
+
+        var consumable = unmarshallConsumable(inputXml);
         var medication = medicationMapper.createMedication(consumable);
 
         verify(codeableConceptMapper, times(1)).mapToCodeableConceptForMedication(any());
@@ -70,7 +95,7 @@ public class MedicationMapperTest {
     }
 
     @SneakyThrows
-    private RCMRMT030101UK04Consumable unmarshallConsumable(String fileName) {
-        return unmarshallFile(getFile("classpath:" + XML_RESOURCES_BASE + fileName), RCMRMT030101UK04Consumable.class);
+    private RCMRMT030101UK04Consumable unmarshallConsumable(String inputXml) {
+        return unmarshallString(inputXml, RCMRMT030101UK04Consumable.class);
     }
 }
