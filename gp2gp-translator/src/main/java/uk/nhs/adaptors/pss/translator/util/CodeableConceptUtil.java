28f4571296a4b0ad41a7588c527f069a75ebbc23
MartinWheelerMT <martin.wheeler@madetech.com>
2024-03-20 16:01:50 +0000

    Refactor CodeableConceptUtil

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/CodeableConceptUtil.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/CodeableConceptUtil.java
@@ -11,54 +11,42 @@ public final class CodeableConceptUtil {
 
     public static Boolean compareCodeableConcepts(CD c1, CD c2) {
 
-        if (c1 == null && c2 == null) {
-            return Boolean.TRUE;
-        }
-
         if (c1 == null || c2 == null) {
-            return Boolean.FALSE;
+            return c1 == c2;
         }
 
-        var c1Qualifier = c1.getQualifier();
-        var c2Qualifier = c2.getQualifier();
-        var c1Translation = c1.getTranslation();
-        var c2Translation = c2.getTranslation();
-
-        return  Objects.equals(c1.getCode(), c2.getCode())
-                 && Objects.equals(c1.getCodeSystem(), c2.getCodeSystem())
-                 && Objects.equals(c1.getDisplayName(), c2.getDisplayName())
-                 && Objects.equals(c1.getOriginalText(), c2.getOriginalText())
-                 && (c1Qualifier == null && c2Qualifier == null || compareCodeSubElements(c1Qualifier, c2Qualifier))
-                 && (c1Translation == null && c2Translation == null || compareCodeSubElements(c1Translation, c2Translation));
+        return Objects.equals(c1.getCode(), c2.getCode())
+                && Objects.equals(c1.getCodeSystem(), c2.getCodeSystem())
+                && Objects.equals(c1.getDisplayName(), c2.getDisplayName())
+                && Objects.equals(c1.getOriginalText(), c2.getOriginalText())
+                && compareCodeSubElements(c1.getQualifier(), c2.getQualifier())
+                && compareCodeSubElements(c1.getTranslation(), c2.getTranslation());
     }
 
     private static Boolean compareCodeSubElements(List<? extends CD> c1, List<? extends CD> c2) {
-
-        boolean itemsMatch = Boolean.FALSE;
-
-        if ((c1 == null && c2 == null) || (c1.isEmpty() && c2.isEmpty())) {
-            return Boolean.TRUE;
+        if (c1 == null || c2 == null) {
+            return c1 == c2;
         }
 
-        if (c1 != null && c2 != null && c1.size() == c2.size()) {
+        if (c1.isEmpty() && c2.isEmpty()) {
+            return true;
+        }
 
-            for (CD c1Element : c1) {
-                for (CD c2Element : c2) {
-                    if (compareCodeableConcepts(c1Element, c2Element)) {
-                        itemsMatch = true;
-                        break;
-                    } else {
-                        itemsMatch = false;
-                    }
-                }
+        if (c1.size() != c2.size()) {
+            return false;
+        }
 
-                if (!itemsMatch) {
-                    return itemsMatch;
-                }
+        for (CD c1Element : c1) {
+            boolean matchFound = c2
+                    .stream()
+                    .anyMatch(c2Element -> compareCodeableConcepts(c1Element, c2Element));
+
+            if (!matchFound) {
+                return false;
             }
         }
 
-        return itemsMatch;
+        return true;
     }
 
 }
