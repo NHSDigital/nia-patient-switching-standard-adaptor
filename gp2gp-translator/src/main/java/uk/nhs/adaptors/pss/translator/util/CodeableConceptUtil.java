670fa1bdd9628cc4a50177b2dcf4b7ecb9bd881f
hospel <ole4ryb@gmail.com>
2024-03-20 13:40:21 +0000

    introducing variables for better readability

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/CodeableConceptUtil.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/util/CodeableConceptUtil.java
@@ -10,42 +10,55 @@ public final class CodeableConceptUtil {
 
     public static Boolean compareCodeableConcepts(CD c1, CD c2) {
 
+        var c1Code = c1.getCode();
+        var c2Code = c2.getCode();
+        var c1CodeSystem = c1.getCodeSystem();
+        var c2CodeSystem = c2.getCodeSystem();
+        var c1DisplayName = c1.getDisplayName();
+        var c2DisplayName = c2.getDisplayName();
+        var c1OriginalText = c1.getOriginalText();
+        var c2OriginalText = c2.getOriginalText();
+        var c1Qualifier = c1.getQualifier();
+        var c2Qualifier = c2.getQualifier();
+        var c1Translation = c1.getTranslation();
+        var c2Translation = c2.getTranslation();
+
+
+
         if (c1 == null && c2 == null) {
             return Boolean.TRUE;
         }
 
-        if ((c1.getCode() == null && c2.getCode() != null) || c1.getCode() != null && c2.getCode() == null) {
+        if ((c1Code == null && c2Code != null) || c1Code != null && c2Code == null) {
             return Boolean.FALSE;
         }
 
-        if ((c1.getCodeSystem() == null && c2.getCodeSystem() != null) || c1.getCodeSystem() != null && c2.getCodeSystem() == null) {
+        if ((c1CodeSystem == null && c2CodeSystem != null) || c1CodeSystem != null && c2CodeSystem == null) {
             return Boolean.FALSE;
         }
 
-        if ((c1.getDisplayName() == null && c2.getDisplayName() != null) || c1.getDisplayName() != null && c2.getDisplayName() == null) {
+        if ((c1DisplayName == null && c2DisplayName != null) || c1DisplayName != null && c2DisplayName == null) {
             return Boolean.FALSE;
         }
 
-        if ((c1.getOriginalText() == null && c2.getOriginalText() != null)
-                                                                         || c1.getOriginalText() != null && c2.getOriginalText() == null) {
+        if ((c1OriginalText == null && c2OriginalText != null) || c1OriginalText != null && c2OriginalText == null) {
             return Boolean.FALSE;
         }
 
-        if ((c1.getQualifier() == null && c2.getQualifier() != null) || c1.getQualifier() != null && c2.getQualifier() == null) {
+        if ((c1Qualifier == null && c2Qualifier != null) || c1Qualifier != null && c2Qualifier == null) {
             return Boolean.FALSE;
         }
 
-        if ((c1.getTranslation() == null && c2.getTranslation() != null) || c1.getTranslation() != null && c2.getTranslation() == null) {
+        if ((c1Translation == null && c2Translation != null) || c1Translation != null && c2Translation == null) {
             return Boolean.FALSE;
         }
 
-        return (c1.getCode() == null && c2.getCode() == null) || c1.getCode().equals(c2.getCode())
-               && (c1.getCodeSystem() == null && c2.getCodeSystem() == null || c1.getCodeSystem().equals(c2.getCodeSystem()))
-               && (c1.getCodeSystem() == null && c2.getCodeSystem() == null || c1.getDisplayName().equals(c2.getDisplayName()))
-               && (c1.getCodeSystem() == null && c2.getCodeSystem() == null || c1.getOriginalText().equals(c2.getOriginalText()))
-               && (c1.getQualifier() == null && c2.getQualifier() == null || compareCodeSubelements(c1.getQualifier(), c2.getQualifier()))
-               && (c1.getTranslation() == null && c2.getTranslation() == null
-                                                                    || compareCodeSubelements(c1.getTranslation(), c2.getTranslation()));
+        return (c1Code == null && c2Code == null) || c1Code.equals(c2Code)
+               && (c1CodeSystem == null && c2CodeSystem == null || c1CodeSystem.equals(c2CodeSystem))
+               && (c1DisplayName == null && c2DisplayName == null || c1DisplayName.equals(c2DisplayName))
+               && (c1OriginalText == null && c2OriginalText == null || c1OriginalText.equals(c2OriginalText))
+               && (c1Qualifier == null && c2Qualifier == null || compareCodeSubelements(c1Qualifier, c2Qualifier))
+               && (c1Translation == null && c2Translation == null || compareCodeSubelements(c1Translation, c2Translation));
     }
 
     private static Boolean compareCodeSubelements(List<? extends CD> c1, List<? extends CD> c2) {
