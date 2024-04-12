8ab54c24178e06ffee9f421fa76aec45fca1479b
azam145 <azam145@gmail.com>
2024-04-12 16:13:35 +0100

    Actioned review comments, factorised checks for dupicate observation statement.

--- gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/DuplicateObservationStatementMapper.java
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/mapper/DuplicateObservationStatementMapper.java
@@ -30,56 +30,43 @@ public class DuplicateObservationStatementMapper {
 
     public void mergeDuplicateObservationStatements(RCMRMT030101UK04EhrExtract ehrExtract) {
         ehrExtract.getComponent()
-            .stream()
-            .map(RCMRMT030101UKComponent::getEhrFolder)
-            .map(RCMRMT030101UKEhrFolder::getComponent)
-            .flatMap(List::stream)
-            .map(RCMRMT030101UKComponent3::getEhrComposition)
-            .forEach(DuplicateObservationStatementMapper::mergeDuplicateObservationStatements);
+                .stream()
+                .map(RCMRMT030101UKComponent::getEhrFolder)
+                .map(RCMRMT030101UKEhrFolder::getComponent)
+                .flatMap(List::stream)
+                .map(RCMRMT030101UKComponent3::getEhrComposition)
+                .forEach(DuplicateObservationStatementMapper::mergeDuplicateObservationStatements);
     }
 
     private static void mergeDuplicateObservationStatements(RCMRMT030101UKEhrComposition ehrComposition) {
         getLinksetsIn(ehrComposition).stream()
-            .map(id -> getLinkedObservationStatement(ehrComposition, id))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .filter(DuplicateObservationStatementMapper::hasSinglePertinentInformation)
-            .filter(DuplicateObservationStatementMapper::isAnnotationTruncated)
-            .filter((RCMRMT030101UKObservationStatement obs) -> obs.getPriorityCode() == null
-                    && obs.getUncertaintyCode() == null
-                    && obs.getUncertaintyCode() == null
-                    && obs.getValue() == null
-                    && obs.getInterpretationCode() == null
-                    && obs.getSubject() == null
-                    && (obs.getSpecimen() == null || obs.getSpecimen().isEmpty())
-                    && (obs.getReferenceRange() == null || obs.getReferenceRange().isEmpty())
-                    && (obs.getInformant() == null || obs.getInformant().isEmpty())
-                    && (obs.getParticipant() == null || obs.getParticipant().isEmpty())
-                    && (obs.getReplacementOf() == null || obs.getReplacementOf().isEmpty())
-                    && (obs.getReason() == null || obs.getReason().isEmpty())
-                    && (obs.getReference() == null || obs.getReference().isEmpty())
-                    && (obs.getSequelTo() == null || obs.getSequelTo().isEmpty()))
+                .map(id -> getLinkedObservationStatement(ehrComposition, id))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .filter(DuplicateObservationStatementMapper::hasSinglePertinentInformation)
+                .filter(DuplicateObservationStatementMapper::isAnnotationTruncated)
+                .filter(DuplicateObservationStatementMapper::isMergeAllowedLinkedStatement)
                 .forEach(truncatedObservationStatement ->
-                findAndMergeNontruncatedObservationStatementIntoTruncatedObservationStatement(
-                    truncatedObservationStatement,
-                    ehrComposition.getComponent()
-                ));
+                        findAndMergeNonTruncatedObservationStatementIntoTruncatedObservationStatement(
+                                truncatedObservationStatement,
+                                ehrComposition.getComponent()
+                        ));
     }
 
     @NotNull
     private static List<String> getLinksetsIn(RCMRMT030101UKEhrComposition ehrComposition) {
         return ehrComposition.getComponent().stream()
-            .map(RCMRMT030101UKComponent4::getLinkSet)
-            .filter(Objects::nonNull)
-            .map(RCMRMT030101UKLinkSet::getConditionNamed)
-            .filter(Objects::nonNull)
-            .map(RCMRMT030101UKConditionNamed::getNamedStatementRef)
-            .map(RCMRMT030101UKStatementRef::getId)
-            .map(II::getRoot)
-            .toList();
+                .map(RCMRMT030101UKComponent4::getLinkSet)
+                .filter(Objects::nonNull)
+                .map(RCMRMT030101UKLinkSet::getConditionNamed)
+                .filter(Objects::nonNull)
+                .map(RCMRMT030101UKConditionNamed::getNamedStatementRef)
+                .map(RCMRMT030101UKStatementRef::getId)
+                .map(II::getRoot)
+                .toList();
     }
 
-    private static void findAndMergeNontruncatedObservationStatementIntoTruncatedObservationStatement(
+    private static void findAndMergeNonTruncatedObservationStatementIntoTruncatedObservationStatement(
             RCMRMT030101UKObservationStatement truncatedObservationStatement, List<RCMRMT030101UKComponent4> components) {
 
 
@@ -90,37 +77,25 @@ public class DuplicateObservationStatementMapper {
         for (Iterator<RCMRMT030101UKComponent4> observationIterator = components.iterator(); observationIterator.hasNext();) {
             var observationStatement = observationIterator.next().getObservationStatement();
             if (observationStatement != null
-                && !areSameObservationStatements(truncatedObservationStatement, observationStatement)
-                && hasSinglePertinentInformation(observationStatement)
-                && doesTruncatedAnnotationMatchOtherAnnotation(truncatedPertinentAnnotation, getPertinentAnnotation(observationStatement))
-                && observationsAreCodedTheSame(truncatedObservationStatement, observationStatement)
-                && observationStatement.getPriorityCode() == null
-                && observationStatement.getUncertaintyCode() == null
-                && observationStatement.getValue() == null
-                && observationStatement.getInterpretationCode() == null
-                && observationStatement.getSubject() == null
-                && (observationStatement.getSpecimen() == null || observationStatement.getSpecimen().isEmpty())
-                && (observationStatement.getReferenceRange() == null || observationStatement.getReferenceRange().isEmpty())
-                && (observationStatement.getInformant() == null || observationStatement.getInformant().isEmpty())
-                && (observationStatement.getParticipant() == null || observationStatement.getParticipant().isEmpty())
-                && (observationStatement.getReplacementOf() == null || observationStatement.getReplacementOf().isEmpty())
-                && (observationStatement.getReason() == null || observationStatement.getReason().isEmpty())
-                && (observationStatement.getReference() == null || observationStatement.getReference().isEmpty())
-                && (observationStatement.getSequelTo() == null || observationStatement.getSequelTo().isEmpty())) {
-
+                    && !areSameObservationStatements(truncatedObservationStatement, observationStatement)
+                    && hasSinglePertinentInformation(observationStatement)
+                    && doesTruncatedAnnotationMatchOtherAnnotation(truncatedPertinentAnnotation,
+                          getPertinentAnnotation(observationStatement))
+                    && observationsAreCodedTheSame(truncatedObservationStatement, observationStatement)
+                    && isMergeAllowedtatement(observationStatement)) {
                 observationIterator.remove();
                 truncatedPertinentAnnotation.setText(annotationPrefix + getPertinentAnnotation(observationStatement).getText());
                 LOGGER.info(
-                    "ObservationStatement: '{}' appears to be a truncated version of '{}'.",
-                    truncatedObservationStatement.getId().getRoot(),
-                    observationStatement.getId().getRoot()
+                        "ObservationStatement: '{}' Is truncated version of '{}' and will be merged into a single observation.",
+                        truncatedObservationStatement.getId().getRoot(),
+                        observationStatement.getId().getRoot()
                 );
                 return;
             }
         }
         LOGGER.info(
-            "ObservationStatement: '{}' appears to have been truncated but no match was found.",
-            truncatedObservationStatement.getId().getRoot()
+                "ObservationStatement: '{}' appears to have been truncated but no match was found.",
+                truncatedObservationStatement.getId().getRoot()
         );
     }
 
@@ -146,8 +121,8 @@ public class DuplicateObservationStatementMapper {
             RCMRMT030101UKAnnotation truncatedPertinentAnnotation, RCMRMT030101UKAnnotation candidateMatchingPertinentAnnotation) {
         String truncatedObservationText = truncatedPertinentAnnotation.getText();
         String truncatedObservationTextWithoutEllipses = truncatedObservationText.substring(
-            truncatedObservationText.length() - CHAR_LIMIT_FOR_TRUNCATION,
-            truncatedObservationText.length() - ELLIPSES.length()
+                truncatedObservationText.length() - CHAR_LIMIT_FOR_TRUNCATION,
+                truncatedObservationText.length() - ELLIPSES.length()
         );
         return candidateMatchingPertinentAnnotation.getText().contains(truncatedObservationTextWithoutEllipses);
     }
@@ -157,15 +132,47 @@ public class DuplicateObservationStatementMapper {
         return annotationText.endsWith(ELLIPSES) && annotationText.length() >= CHAR_LIMIT_FOR_TRUNCATION;
     }
 
+    private static boolean isMergeAllowedLinkedStatement(RCMRMT030101UKObservationStatement obs) {
+        return (obs.getPriorityCode() == null
+                && obs.getUncertaintyCode() == null
+                && obs.getValue() == null
+                && obs.getInterpretationCode() == null
+                && obs.getSubject() == null
+                && (obs.getSpecimen() == null || obs.getSpecimen().isEmpty())
+                && (obs.getReferenceRange() == null || obs.getReferenceRange().isEmpty())
+                && (obs.getInformant() == null || obs.getInformant().isEmpty())
+                && (obs.getParticipant() == null || obs.getParticipant().isEmpty())
+                && (obs.getReplacementOf() == null || obs.getReplacementOf().isEmpty())
+                && (obs.getReason() == null || obs.getReason().isEmpty())
+                && (obs.getReference() == null || obs.getReference().isEmpty())
+                && (obs.getSequelTo() == null || obs.getSequelTo().isEmpty()));
+    }
+
+    private static boolean isMergeAllowedtatement(RCMRMT030101UKObservationStatement observationStatement) {
+        return  observationStatement.getPriorityCode() == null
+                && observationStatement.getUncertaintyCode() == null
+                && observationStatement.getValue() == null
+                && observationStatement.getInterpretationCode() == null
+                && observationStatement.getSubject() == null
+                && (observationStatement.getSpecimen() == null || observationStatement.getSpecimen().isEmpty())
+                && (observationStatement.getReferenceRange() == null || observationStatement.getReferenceRange().isEmpty())
+                && (observationStatement.getInformant() == null || observationStatement.getInformant().isEmpty())
+                && (observationStatement.getParticipant() == null || observationStatement.getParticipant().isEmpty())
+                && (observationStatement.getReplacementOf() == null || observationStatement.getReplacementOf().isEmpty())
+                && (observationStatement.getReason() == null || observationStatement.getReason().isEmpty())
+                && (observationStatement.getReference() == null || observationStatement.getReference().isEmpty())
+                && (observationStatement.getSequelTo() == null || observationStatement.getSequelTo().isEmpty());
+    }
+
     @NotNull
     private static Optional<RCMRMT030101UKObservationStatement> getLinkedObservationStatement(
             RCMRMT030101UKEhrComposition ehrComposition, String observationIdReferencedFromLinkset) {
 
         return ehrComposition.getComponent()
-            .stream()
-            .map(RCMRMT030101UKComponent4::getObservationStatement)
-            .filter(Objects::nonNull)
-            .filter(observationStatement -> Objects.equals(observationIdReferencedFromLinkset, observationStatement.getId().getRoot()))
-            .findFirst();
+                .stream()
+                .map(RCMRMT030101UKComponent4::getObservationStatement)
+                .filter(Objects::nonNull)
+                .filter(observationStatement -> Objects.equals(observationIdReferencedFromLinkset, observationStatement.getId().getRoot()))
+                .findFirst();
     }
 }
