ac1ecb2a0981665f782bc5ff47172d9be80500fb
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-05-20 11:38:37 +0100

    NackAckPrepServiceTest class update

--- gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/service/NackAckPrepServiceTest.java
+++ gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/service/NackAckPrepServiceTest.java
@@ -3,45 +3,37 @@ package uk.nhs.adaptors.pss.translator.service;
 
 import org.hl7.v3.COPCIN000001UK01Message;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.nhs.adaptors.pss.translator.mhs.MhsRequestBuilder;
 import uk.nhs.adaptors.pss.translator.model.ACKMessageData;
-import uk.nhs.adaptors.pss.translator.model.NACKMessageData;
 import uk.nhs.adaptors.pss.translator.task.SendACKMessageHandler;
-import uk.nhs.adaptors.pss.translator.task.SendNACKMessageHandler;
 import uk.nhs.adaptors.pss.translator.util.XmlParseUtilService;
-
-import java.sql.SQLException;
-import javax.xml.bind.JAXBException;
 
 import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.spy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class NackAckPreparationServiceTest {
+@RunWith(MockitoJUnitRunner.class)
+public class NackAckPrepServiceTest {
 
     @Autowired
     private NackAckPreparationService nackAckPreparationService;
 
 
     private static final String CONVERSATION_ID = randomUUID().toString();
-
-    //@Mock
-    //private SendACKMessageHandler sendACKMessageHandler;
 
     @MockBean
     private MhsRequestBuilder requestBuilder;
@@ -74,13 +66,10 @@ public class NackAckPreparationServiceTest {
             () -> XmlParseUtilService.parseFromAsid(any(COPCIN000001UK01Message.class))
                                       ).thenReturn("FromAsid");
 
-        //doThrow(new Exception()).when(sendACKMessageHandler).prepareAndSendMessage(any());
-        //when(sendACKMessageHandler.prepareAndSendMessage(any())).thenThrow(new RuntimeException("msg"));
+        SendACKMessageHandler sendACKMessageHandler = mock(SendACKMessageHandler.class);
+        given(sendACKMessageHandler.prepareAndSendMessage(any())).willAnswer(invocation -> { throw new Exception("abc msg"); });
 
-        SendACKMessageHandler sendACKMessageHandler = spy(SendACKMessageHandler.class);
-        when(sendACKMessageHandler.prepareAndSendMessage(any())).thenThrow(new SQLException("SQLException"));
-
-        Exception exception = assertThrows(RuntimeException.class, () -> {
+        Exception exception = assertThrows(Exception.class, () -> {
             nackAckPreparationService.sendAckMessage(mockCOPCMessage, CONVERSATION_ID, "LoloPractice");
         });
 
