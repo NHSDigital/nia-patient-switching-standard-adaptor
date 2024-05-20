3a8b8e462d1bb08219fd34b34f2205bce4e32da2
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-05-20 15:46:21 +0100

    fixing tests

--- gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/service/NackAckPrepServiceTest.java
+++ gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/service/NackAckPrepServiceTest.java
@@ -3,14 +3,15 @@ package uk.nhs.adaptors.pss.translator.service;
 
 import org.hl7.v3.COPCIN000001UK01Message;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.nhs.adaptors.connector.service.MigrationStatusLogService;
 import uk.nhs.adaptors.pss.translator.mhs.MhsRequestBuilder;
 import uk.nhs.adaptors.pss.translator.model.ACKMessageData;
 import uk.nhs.adaptors.pss.translator.task.SendACKMessageHandler;
@@ -19,21 +20,25 @@ import uk.nhs.adaptors.pss.translator.util.XmlParseUtilService;
 import static java.util.UUID.randomUUID;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class NackAckPrepServiceTest {
+
+    @MockBean
+    private SendACKMessageHandler sendACKMessageHandler;
 
     @Autowired
     private NackAckPreparationService nackAckPreparationService;
 
-
     private static final String CONVERSATION_ID = randomUUID().toString();
+
+    @MockBean
+    private MigrationStatusLogService migrationStatusLogService;
 
     @MockBean
     private MhsRequestBuilder requestBuilder;
@@ -44,11 +49,10 @@ public class NackAckPrepServiceTest {
     @MockBean
     private ApplicationAcknowledgementMessageService messageService;
 
-    @MockBean
-    private IdGeneratorService idGeneratorService;
-
     @Mock
     private COPCIN000001UK01Message mockCOPCMessage;
+
+    private static final int NUMBER_OF_RETRIES = 3;
 
     @Test
     public void When_SendAckMessageRCMR_WithNoErrors_Expect_SomeRetries() {
@@ -66,14 +70,13 @@ public class NackAckPrepServiceTest {
             () -> XmlParseUtilService.parseFromAsid(any(COPCIN000001UK01Message.class))
                                       ).thenReturn("FromAsid");
 
-        SendACKMessageHandler sendACKMessageHandler = mock(SendACKMessageHandler.class);
-        given(sendACKMessageHandler.prepareAndSendMessage(any())).willAnswer(invocation -> { throw new Exception("abc msg"); });
+        doThrow(new RuntimeException("")).when(sendACKMessageHandler).prepareAndSendMessage(any());
 
         Exception exception = assertThrows(Exception.class, () -> {
-            nackAckPreparationService.sendAckMessage(mockCOPCMessage, CONVERSATION_ID, "LoloPractice");
+            nackAckPreparationService.sendAckMessage(mockCOPCMessage, CONVERSATION_ID, "ColoColoPractice");
         });
 
-        verify(sendACKMessageHandler, times(3))
+        verify(sendACKMessageHandler, times(NUMBER_OF_RETRIES))
                 .prepareAndSendMessage(any(ACKMessageData.class));
     }
 
