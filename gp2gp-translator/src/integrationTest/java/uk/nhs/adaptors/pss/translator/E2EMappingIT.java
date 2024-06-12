5233432973826844d719d52d7c937a5fae1ce88c
MartinWheelerMT <88717465+MartinWheelerMT@users.noreply.github.com>
2024-06-12 17:23:59 +0100

    Refactor e2e mapping test (#560)
    * refactor E2EMappingIT to remove unnecessary bloat
    
    * refactor E2EMappingIT to remove unnecessary bloat
    Move RandomIdGenerator MockBean to E2EMappingIT

--- gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/E2EMappingIT.java
+++ gp2gp-translator/src/integrationTest/java/uk/nhs/adaptors/pss/translator/E2EMappingIT.java
@@ -2,49 +2,42 @@ package uk.nhs.adaptors.pss.translator;
 
 import static org.awaitility.Awaitility.await;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static uk.nhs.adaptors.common.util.FileUtil.readResourceAsString;
-import static uk.nhs.adaptors.pss.translator.util.XmlUnmarshallUtil.unmarshallString;
-import static uk.nhs.adaptors.pss.util.JsonPathIgnoreGeneratorUtil.generateJsonPathIgnores;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Stream;
-
-import javax.xml.bind.JAXBException;
-
-import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.v3.RCMRIN030000UK06Message;
 import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.SneakyThrows;
-import uk.nhs.adaptors.common.util.fhir.FhirParser;
 import uk.nhs.adaptors.connector.dao.PatientMigrationRequestDao;
-import uk.nhs.adaptors.connector.service.MigrationStatusLogService;
 import uk.nhs.adaptors.pss.translator.mhs.model.InboundMessage;
+import uk.nhs.adaptors.pss.translator.service.IdGeneratorService;
 import uk.nhs.adaptors.pss.util.BaseEhrHandler;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
+@DirtiesContext
 public class E2EMappingIT extends BaseEhrHandler {
-
-    private static final String PSS_ADAPTOR_URL = "https://PSSAdaptor/";
     private static final String EBXML_PART_PATH = "/xml/RCMR_IN030000UK06/ebxml_part.xml";
-    //these are programming language special characters, not to be confused with line endings
-    private static final String SPECIAL_CHARS = "\\\\n|\\\\t|\\\\b|\\\\r";
-
-    private String nhsNumberToBeReplaced;
+    public static final String INPUT_RESOURCE_PATH = "/e2e-mapping/input-xml/";
+    public static final String OUTPUT_RESOURCE_PATH = "/e2e-mapping/output-json/";
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -54,286 +47,137 @@ public class E2EMappingIT extends BaseEhrHandler {
     private JmsTemplate mhsJmsTemplate;
 
     @Autowired
-    private MigrationStatusLogService migrationStatusLogService;
-
-    @Autowired
     private PatientMigrationRequestDao patientMigrationRequestDao;
 
-    @Autowired
-    private FhirParser fhirParserService;
+    @MockBean
+    private IdGeneratorService idGeneratorService;
+
+    @BeforeEach
+    public void setUpDeterministicRandomIds() {
+        when(idGeneratorService.generateUuid()).thenAnswer(
+                new Answer<String>() {
+                    private int invocationCount = 0;
+                    @Override
+                    public String answer(InvocationOnMock invocation) {
+                        return String.format("00000000-0000-0000-0000-%012d", invocationCount++);
+                    }
+                }
+        );
+    }
 
     @Override
     @BeforeEach
     public void setUp() {
         setPatientNhsNumber(generatePatientNhsNumber());
-        setConversationId(generateConversationId().toUpperCase(Locale.ROOT));
+        setConversationId(generateConversationId().toUpperCase());
         setLosingODSCode("B83002");
         setWiningODSCode("C81007");
         startPatientMigrationJourney();
     }
 
-    private static List<String> staticIgnoredJsonPaths = List.of(
-            "id",
-            "entry[0].resource.id",
-            "entry[0].resource.identifier[0].value",
-            "entry[1].resource.id",
-            "entry[*].resource.subject.reference",
-            "entry[*].resource.patient.reference"
-    );
-
     @Test
-    public void handlePWTP2EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP2";
+    public void handlePWTP2EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP2-output.json");
 
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP2.xml");
 
-        List<String> ignoredFields = List.of(
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[9].resource.location[0].location.reference",
-                "entry[10].resource.location[0].location.reference",
-                "entry[11].resource.location[0].location.reference",
-                "entry[12].resource.location[0].location.reference",
-                "entry[49].resource.id",
-                "entry[49].resource.identifier[0].value",
-                "entry[50].resource.id",
-                "entry[50].resource.identifier[0].value"
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
 
-        );
-
-        executeTest(inputFileName, ignoredFields);
     }
 
     @Test
-    public void handlePWTP3EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP3";
+    public void handlePWTP3EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP3-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[3].resource.location[0].location.reference",
-                "entry[11].resource.id",
-                "entry[11].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP3.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP4EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP4";
+    public void handlePWTP4EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP4-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[9].resource.location[0].location.reference",
-                "entry[10].resource.location[0].location.reference",
-                "entry[11].resource.location[0].location.reference",
-                "entry[44].resource.id",
-                "entry[44].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP4.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP5EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP5";
+    public void handlePWTP5EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP5-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[2].resource.location[0].location.reference",
-                "entry[3].resource.location[0].location.reference",
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[9].resource.location[0].location.reference",
-                "entry[10].resource.location[0].location.reference",
-                "entry[45].resource.id",
-                "entry[45].resource.identifier[0].value"
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP5.xml");
 
-        );
-
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP6EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP6";
+    public void handlePWTP6EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP6-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[3].resource.location[0].location.reference",
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[35].resource.id",
-                "entry[35].resource.identifier[0].value",
-                "entry[36].resource.id",
-                "entry[36].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP6.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP7visEhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP7_vis";
+    public void handlePWTP7visEhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP7_vis-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[9].resource.location[0].location.reference",
-                "entry[10].resource.location[0].location.reference",
-                "entry[11].resource.location[0].location.reference",
-                "entry[12].resource.location[0].location.reference",
-                "entry[13].resource.location[0].location.reference",
-                "entry[14].resource.location[0].location.reference",
-                "entry[48].resource.id",
-                "entry[48].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP7_vis.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP9EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP9";
+    public void handlePWTP9EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP9-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[23].resource.location[0].location.reference",
-                "entry[24].resource.location[0].location.reference",
-                "entry[89].resource.id",
-                "entry[89].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP9.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP10EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP10";
+    public void handlePWTP10EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP10-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[3].resource.location[0].location.reference",
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[9].resource.location[0].location.reference",
-                "entry[10].resource.location[0].location.reference",
-                "entry[48].resource.id",
-                "entry[48].resource.identifier[0].value",
-                "entry[49].resource.id",
-                "entry[49].resource.identifier[0].value"
-        );
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP10.xml");
 
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
     @Test
-    public void handlePWTP11EhrExtractFromQueue() throws JSONException, JAXBException {
-        String inputFileName = "PWTP11";
+    public void handlePWTP11EhrExtractFromQueue() throws JSONException {
+        final var expectedBundle = readResourceAsString(OUTPUT_RESOURCE_PATH + "PWTP11-output.json");
 
-        List<String> ignoredFields = List.of(
-                "entry[2].resource.location[0].location.reference",
-                "entry[3].resource.location[0].location.reference",
-                "entry[4].resource.location[0].location.reference",
-                "entry[5].resource.location[0].location.reference",
-                "entry[6].resource.location[0].location.reference",
-                "entry[7].resource.location[0].location.reference",
-                "entry[8].resource.location[0].location.reference",
-                "entry[48].resource.id",
-                "entry[48].resource.identifier[0].value"
+        final var actualBundle = sendInboundMessageAndWaitForBundle(INPUT_RESOURCE_PATH + "PWTP11.xml");
 
-        );
-
-        executeTest(inputFileName, ignoredFields);
+        JSONAssert.assertEquals(expectedBundle, actualBundle, true);
     }
 
-    private void executeTest(String inputFileName, List<String> ignoredFields) throws JAXBException, JSONException {
-        // process starts with consuming a message from MHS queue
-        sendInboundMessageToQueue("/e2e-mapping/input-xml/" + inputFileName + ".xml");
-        // wait until EHR extract is translated to bundle resource and saved to the DB
+
+    private String sendInboundMessageAndWaitForBundle(String inputFilePath) {
+        final var inboundMessage = parseMessageToString(createInboundMessage(inputFilePath));
+        mhsJmsTemplate.send(session -> session.createTextMessage(inboundMessage));
         await().until(this::isEhrMigrationCompleted);
-        // verify generated bundle resource
-        verifyBundle("/e2e-mapping/output-json/" + inputFileName + "-output.json", ignoredFields);
+
+        var patientMigrationRequest = patientMigrationRequestDao.getMigrationRequest(getConversationId());
+        return patientMigrationRequest.getBundleResource();
     }
 
-    private void sendInboundMessageToQueue(String payloadPartPath) throws JAXBException {
-        var inboundMessage = createInboundMessage(payloadPartPath);
-        mhsJmsTemplate.send(session -> session.createTextMessage(parseMessageToString(inboundMessage)));
-    }
-
-    private InboundMessage createInboundMessage(String payloadPartPath) throws JAXBException {
-
-        var inboundMessage = new InboundMessage();
-
-        var payload = readResourceAsString(payloadPartPath);
+    private InboundMessage createInboundMessage(String payloadPartPath) {
         var ebXml = readResourceAsString(EBXML_PART_PATH).replace(CONVERSATION_ID_PLACEHOLDER, getConversationId());
 
-        RCMRIN030000UK06Message payloadObject = unmarshallString(payload, RCMRIN030000UK06Message.class);
-
-        nhsNumberToBeReplaced = getNhsNumberToBeReplaced(payloadObject);
-
-        inboundMessage.setPayload(payload.replaceAll(nhsNumberToBeReplaced, getPatientNhsNumber()));
+        var inboundMessage = new InboundMessage();
+        inboundMessage.setPayload(readResourceAsString(payloadPartPath));
         inboundMessage.setEbXML(ebXml);
         return inboundMessage;
-    }
-
-    private String getNhsNumberToBeReplaced(RCMRIN030000UK06Message payloadObject) {
-        return payloadObject
-                .getControlActEvent()
-                .getSubject()
-                .getEhrExtract()
-                .getRecordTarget()
-                .getPatient()
-                .getId()
-                .getRoot();
     }
 
     @SneakyThrows
     private String parseMessageToString(InboundMessage inboundMessage) {
         return objectMapper.writeValueAsString(inboundMessage);
     }
-
-
-    protected void verifyBundle(String path, List<String> ignoredFields) throws JSONException {
-        var patientMigrationRequest = patientMigrationRequestDao.getMigrationRequest(getConversationId());
-        var expectedBundle = readResourceAsString(path).replaceAll(nhsNumberToBeReplaced, getPatientNhsNumber());
-        var odsCodeToBeReplaced = getOdsToBeReplaced(expectedBundle);
-
-        if (OVERWRITE_EXPECTED_JSON) {
-            overwriteExpectJson(path, patientMigrationRequest.getBundleResource());
-        }
-
-        var bundle = fhirParserService.parseResource(patientMigrationRequest.getBundleResource(), Bundle.class);
-        var combinedList = Stream.of(generateJsonPathIgnores(bundle), staticIgnoredJsonPaths)
-                .flatMap(List::stream)
-                .toList();
-
-        expectedBundle = expectedBundle.replaceAll(odsCodeToBeReplaced, this.getLosingODSCode()).replaceAll(SPECIAL_CHARS, "");
-        var actualBundle = patientMigrationRequest.getBundleResource().replaceAll(SPECIAL_CHARS, "");
-
-        assertBundleContent(
-                actualBundle,
-                expectedBundle,
-                Stream.concat(combinedList.stream(), ignoredFields.stream()).toList()
-        );
-    }
-
-    private String getOdsToBeReplaced(String expectedBundle) {
-        var startIndex = expectedBundle.toLowerCase().indexOf(PSS_ADAPTOR_URL.toLowerCase()) + PSS_ADAPTOR_URL.length();
-        var endIndex = expectedBundle.toLowerCase().indexOf("\"", startIndex);
-
-        return expectedBundle.substring(startIndex, endIndex);
-    }
-
 }
