35e05755eaf6c889dbabd74f098af4879e1ef071
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-06-25 11:51:31 +0100

    [NIAD-3113] Refactor unit tests

--- schema/src/test/java/JaxbTest.java
+++ schema/src/test/java/JaxbTest.java
@@ -12,14 +12,13 @@ import org.junit.jupiter.api.Test;
 
 public class JaxbTest {
 
-    public static final File TEST_FILE = new File("src/test/resources/xml/9465698490_Daniels_full_20210602.xml");
+    public static final File TEST_FILE = new File("src/test/resources/xml/RCMR_IN030000UK06.xml");
 
     @Test
-    public void testUnmarshal() throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(RCMRIN030000UK06Message.class);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-        JAXBElement<RCMRIN030000UK06Message> unmarshalledMessage = (JAXBElement) unmarshaller.unmarshal(TEST_FILE);
+    void When_RCMRIN030000UK06MessageIsUnmarshalled_Expect_LooksOk() throws JAXBException {
+        // given
+        final JAXBElement<RCMRIN030000UK06Message> unmarshalledMessage
+            = unmarshallFromFile(TEST_FILE, RCMRIN030000UK06Message.class);
 
         var ehrFolder = unmarshalledMessage
             .getValue()
@@ -38,5 +37,13 @@ public class JaxbTest {
         assertThat(person.getName().getPrefix()).isEqualTo("Dr");
         assertThat(person.getName().getValidTime().getCenter().getValue()).isEqualTo("20100114");
         assertThat(place.getName()).isEqualTo("EMIS Test Practice Location");
+    }
+
+    // Helper Methods
+    private <T> JAXBElement<T> unmarshallFromFile(final File source, Class<T> target) throws JAXBException {
+        final JAXBContext jaxbContext = JAXBContext.newInstance(target);
+        final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        return (JAXBElement<T>) unmarshaller.unmarshal(source);
     }
 }
