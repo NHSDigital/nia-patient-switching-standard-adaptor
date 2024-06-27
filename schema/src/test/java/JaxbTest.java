92ffc39f330d71237cd2e33fbeade094047da0de
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-06-27 20:34:44 +0100

    [NIAD-3029-MINIMAL-IMPACT] Started adding tests and refactoring current fields.

--- schema/src/test/java/JaxbTest.java
+++ schema/src/test/java/JaxbTest.java
@@ -2,24 +2,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import org.hl7.v3.RCMRIN030000UK06Message;
 import org.junit.jupiter.api.Test;
 
-public class JaxbTest {
-
-    public static final File TEST_FILE = new File("src/test/resources/xml/9465698490_Daniels_full_20210602.xml");
+class JaxbTest {
+    private static final String BASE_FILE_PATH = "src/test/resources/xml/";
+    private static final File RCMR_IN030000UK06_TEST_FILE = new File(BASE_FILE_PATH + "RCMR_IN030000UK06.xml");
+    private static final File RCMR_IN030000UK07_TEST_FILE = new File(BASE_FILE_PATH + "RCMR_IN030000UK07.xml");
 
     @Test
-    public void testUnmarshal() throws JAXBException {
+    void When_RCMRIN030000UK07MessageIsUnmarshalled_Expect_FieldsToBeParsable() throws JAXBException {
         JAXBContext jaxbContext = JAXBContext.newInstance(RCMRIN030000UK06Message.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
-        JAXBElement<RCMRIN030000UK06Message> unmarshalledMessage = (JAXBElement) unmarshaller.unmarshal(TEST_FILE);
+        JAXBElement<RCMRIN030000UK06Message> unmarshalledMessage = (JAXBElement) unmarshaller.unmarshal(RCMR_IN030000UK06_TEST_FILE);
 
         var ehrFolder = unmarshalledMessage
             .getValue()
