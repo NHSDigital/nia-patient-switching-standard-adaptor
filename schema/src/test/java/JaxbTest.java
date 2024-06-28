6805dea18da1b8e2a0955dbb87b1d78b6c6a1bac
Adrian Clay <adrian.clay1@nhs.net>
2024-06-28 10:48:16 +0100

    Run the schema tests within Jenkins (#661)
    * Run the schema tests within Jenkins
    * Fix Schema tests with javax -> jakarta package update

--- schema/src/test/java/JaxbTest.java
+++ schema/src/test/java/JaxbTest.java
@@ -2,10 +2,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 import org.hl7.v3.RCMRIN030000UK06Message;
 import org.junit.jupiter.api.Test;
