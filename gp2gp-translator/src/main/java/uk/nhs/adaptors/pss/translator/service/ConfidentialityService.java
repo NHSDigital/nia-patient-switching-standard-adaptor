8ea91f46fb6004d5e70439f0fedf5d8fef11d9f1
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-07-11 16:00:12 +0100

    [NIAD-3132] Added initial tests for ConfidentialityService.java

---
+++ gp2gp-translator/src/main/java/uk/nhs/adaptors/pss/translator/service/ConfidentialityService.java
@@ -0,0 +1,43 @@
+package uk.nhs.adaptors.pss.translator.service;
+
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Meta;
+import org.hl7.v3.CV;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class ConfidentialityService {
+    private static final Coding CONFIDENTIALITY_CODING = new Coding()
+        .setSystem("http://hl7.org/fhir/v3/ActCode")
+        .setCode("NOPAT")
+        .setDisplay("no disclosure to patient, family or caregivers without attending provider's authorization");
+
+    public Meta addSecurityToMetaIfConfidentialityCodesPresent(Collection<Optional<CV>> confidentialityCodes, Meta meta) {
+        final boolean isCodePresent = confidentialityCodes.stream()
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .anyMatch(this::isNopat);
+
+        if (isCodePresent) {
+            return addConfidentialityToMeta(meta);
+        }
+
+        return meta;
+    }
+
+    private Meta addConfidentialityToMeta(final Meta meta) {
+        return meta.setSecurity(
+            Collections.singletonList(
+                CONFIDENTIALITY_CODING
+            )
+        );
+    }
+
+    private boolean isNopat(CV coding) {
+        return coding.getCode().equals("NOPAT");
+    }
+}
