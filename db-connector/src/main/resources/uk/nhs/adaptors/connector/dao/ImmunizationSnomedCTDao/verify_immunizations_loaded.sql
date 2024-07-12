e3fc3e6c6a3fc9100b0d6182736a5c8e6e4c7c6e
MartinWheelerMT <88717465+MartinWheelerMT@users.noreply.github.com>
2024-07-12 08:24:23 +0100

    NIAD-3013:  Throw exception on application startup if immunization codes are not loaded (#694)
    * Add sql command to ImmunizationSnomedCTDao to check if immunization codes have been loaded
    Add custom CommandLineRunner which will be executed after runtime initialization
    Add Unit tests to ensure that the CommandLineRunner throws an exception when required.
    
    * Update CHANGELOG.md
    
    * Update README.md for snomed-database-loader
    
    * Correct test name in ApplicationCommandLineRunnerTest
    
    * Add comment to load-release-postgressql.sh explaining the coupling between this script and the translator service
    Refactored the REFRESH MATERIALIZED VIEWS so both are completed in the same PSQL command
    
    * Address issues in PR comments

---
+++ db-connector/src/main/resources/uk/nhs/adaptors/connector/dao/ImmunizationSnomedCTDao/verify_immunizations_loaded.sql
@@ -0,0 +1,11 @@
+-- the conceptIds being checked for below represent the immunization root codes and those immunization codes outside
+-- of the root code hierarchy, as described in the snomed-database-loader README.md file
+
+SELECT COUNT(i.conceptid) = 16 -- total number of codes
+FROM "snomedct".immunization_codes i
+WHERE i.conceptid IN (
+                      '787859002', '127785005', '304250009', '90351000119108','713404003','2997511000001102',
+                      '308101000000104','1036721000000101', '1373691000000102', '945831000000105', '542931000000103',
+                      '735981009', '90640007', '571631000119106', '764141000000106', '170399005'
+                     );
+
