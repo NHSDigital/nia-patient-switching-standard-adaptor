8c093883617f17256b46cd23f8f56203088b9b57
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-08-08 13:35:01 +0100

    [NIAD-3148] Add method to TestUtility.java `getEhrFolderComponent`

--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/TestUtility.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/TestUtility.java
@@ -1,19 +1,22 @@
 package uk.nhs.adaptors.pss.translator;
 
 import org.hl7.v3.CV;
+import org.hl7.v3.RCMRMT030101UKComponent3;
 import org.hl7.v3.RCMRMT030101UKEhrComposition;
 import org.hl7.v3.RCMRMT030101UKEhrExtract;
+import org.hl7.v3.RCMRMT030101UKEhrFolder;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public final class TestUtility {
     private TestUtility() { }
 
     public static final Function<RCMRMT030101UKEhrExtract, RCMRMT030101UKEhrComposition> GET_EHR_COMPOSITION =
         extract -> extract
-            .getComponent().get(0)
+            .getComponent().getFirst()
             .getEhrFolder()
-            .getComponent().get(0)
+            .getComponent().getFirst()
             .getEhrComposition();
 
     public static CV createCv(String code, String codeSystem, String displayName) {
@@ -28,10 +31,41 @@ public final class TestUtility {
         return createCv(code, "", "");
     }
 
+    /**
+     * An EHR Extract has a cardinality of one to many components, each component (based
+     * of the UK05 schema) can contain one and only one EHR Folder. This utility method provides
+     * a means of extracting a single component from within a target EHR Folder based on a predicate.
+     * @param extract The EHR Extract to be filtered.
+     * @param extractComponentIndex The index of the component which houses the EHR Folder.
+     * @param searchCriteriaPredicate The predicate to find the component.
+     * @return The found RCMRMT030101UKComponent3, or else a ComponentDoesNotExistException will be thrown.
+     */
+    public static RCMRMT030101UKComponent3 getEhrFolderComponent(RCMRMT030101UKEhrExtract extract,
+                                                                 int extractComponentIndex,
+                                                                 Predicate<RCMRMT030101UKComponent3> searchCriteriaPredicate) {
+        final RCMRMT030101UKEhrFolder targetFolder = extract
+            .getComponent()
+            .get(extractComponentIndex)
+            .getEhrFolder();
+
+        return targetFolder.getComponent().stream()
+            .filter(searchCriteriaPredicate)
+            .findFirst()
+            .orElseThrow(ComponentDoesNotExistException::new);
+    }
+
     public static class NoConfidentialityCodePresentException extends RuntimeException {
         private static final String EXCEPTION_MESSAGE = "No confidentiality code is present within the test file.";
 
         public NoConfidentialityCodePresentException() {
+            super(EXCEPTION_MESSAGE);
+        }
+    }
+
+    public static class ComponentDoesNotExistException extends RuntimeException {
+        private static final String EXCEPTION_MESSAGE = "No component could not be found.";
+
+        public ComponentDoesNotExistException() {
             super(EXCEPTION_MESSAGE);
         }
     }
