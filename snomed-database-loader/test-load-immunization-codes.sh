6f65eb26c14776c809ac50d78d808bcd2f353fa9
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-08-12 10:24:02 +0100

    [NIAD-3148] Changed test-load-immunization-codes.sh, now if a given environment variable is not present, it will display a summary of all variables not present, saving time.

--- snomed-database-loader/test-load-immunization-codes.sh
+++ snomed-database-loader/test-load-immunization-codes.sh
@@ -8,29 +8,13 @@ rootImmunizationCodes=('787859002' '127785005' '304250009' '90351000119108' '713
 childImmunizationCodes=('2997511000001102' '308101000000104' '1036721000000101' '1373691000000102' '945831000000105')
 immunizationCodesNotInImmunizationHierarchy=('542931000000103' '735981009' '90640007' '571631000119106' '764141000000106' '170399005' )
 
-
-if [ -z ${PS_DB_OWNER_NAME} ]
-then
-  echo "Please set the following env var: PS_DB_OWNER_NAME, e.g. \"export PS_DB_OWNER_NAME='postgres'\""
-	exit 1
-fi
-
-if [ -z ${PS_DB_HOST} ]
-then
-  echo "Please set the following env var: PS_DB_HOST, e.g. \"export PS_DB_HOST='localhost'\""
-	exit 1
-fi
-
-if [ -z ${PS_DB_PORT} ]
-then
-  echo "Please set the following env var: PS_DB_PORT, e.g. \"export PS_DB_PORT='5432'\""
-	exit 1
-fi
-
-if [ -z ${POSTGRES_PASSWORD} ]
-then
-  echo "Please set the following env var: POSTGRES_PASSWORD, e.g. \"export POSTGRES_PASSWORD='********'\""
-	exit 1
+if [ -z ${PS_DB_OWNER_NAME} ] || [ -z ${PS_DB_HOST} ] || [ -z ${PS_DB_PORT} ] || [ -z ${POSTGRES_PASSWORD} ]; then
+  echo "The following environment variables are required for this script:"
+  [ -z ${PS_DB_OWNER_NAME} ] && echo "> PS_DB_OWNER_NAME, e.g. \"export PS_DB_OWNER_NAME='postgres'\""
+  [ -z ${PS_DB_HOST} ] && echo "> PS_DB_HOST, e.g. \"export PS_DB_HOST='localhost'\""
+  [ -z ${PS_DB_PORT} ] && echo "> PS_DB_PORT, e.g. \"export PS_DB_PORT='5432'\""
+  [ -z ${POSTGRES_PASSWORD} ] && echo "> POSTGRES_PASSWORD, e.g. \"export POSTGRES_PASSWORD='********'\""
+  exit 1
 fi
 
 databaseUri="postgresql://${PS_DB_OWNER_NAME}:${POSTGRES_PASSWORD}@${PS_DB_HOST}:${PS_DB_PORT}/${dbName}"
