89041134ae772d8530c2309339edb3a11da7032b
MartinWheelerMT <martin.wheeler@madetech.com>
2024-09-20 10:13:19 +0100

    NIAD-3170: Add Baseline tests for MedicationRequest Plans Add a test to confirm current functionality works as expected for non-acute plans. Add a test to confirm that the earliest order is based on the already present plan.

--- gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationRequestMapperTest.java
+++ gp2gp-translator/src/test/java/uk/nhs/adaptors/pss/translator/mapper/medication/MedicationRequestMapperTest.java
@@ -1,11 +1,16 @@
 package uk.nhs.adaptors.pss.translator.mapper.medication;
 
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.DateTimeType;
 import org.hl7.fhir.dstu3.model.DomainResource;
+import org.hl7.fhir.dstu3.model.Extension;
+import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Medication;
 import org.hl7.fhir.dstu3.model.MedicationRequest;
 import org.hl7.fhir.dstu3.model.MedicationStatement;
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.dstu3.model.Period;
 import org.hl7.fhir.dstu3.model.Reference;
 import org.hl7.fhir.dstu3.model.ResourceType;
 import org.hl7.v3.RCMRMT030101UKAuthorise;
@@ -23,6 +28,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hl7.fhir.dstu3.model.MedicationRequest.MedicationRequestDispenseRequestComponent;
+import static org.hl7.fhir.dstu3.model.MedicationRequest.MedicationRequestIntent;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -50,6 +58,31 @@ public class MedicationRequestMapperTest {
     private static final DateTimeType EXPECTED_DATE_TIME_TYPE = DateFormatUtil.parseToDateTimeType("20100115");
 
     private static final String PATIENT_ID = "d7d4ab01-c3a9-4120-9364-4a5b3fd614d0";
+
+    private static final Extension ACUTE_PRESCRIPTION_EXTENSION = new Extension(
+        "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-PrescriptionType-1",
+        new CodeableConcept(
+            new Coding("https://fhir.nhs.uk/STU3/CodeSystem/CareConnect-PrescriptionType-1",
+                "acute",
+                "Acute"
+            )
+        )
+    );
+    private static final Extension REPEAT_PRESCRIPTION_EXTENSION = new Extension(
+        "https://fhir.nhs.uk/STU3/StructureDefinition/Extension-CareConnect-GPC-PrescriptionType-1",
+        new CodeableConcept(
+            new Coding("https://fhir.nhs.uk/STU3/CodeSystem/CareConnect-PrescriptionType-1",
+                "repeat",
+                "Repeat"
+            )
+        )
+    );
+    private static final Reference REFERENCE_TO_PLAN = new Reference(
+        new IdType(
+            ResourceType.MedicationRequest.name(),
+            "000EEA41-289B-4B1C-A5AB-421A666A0D2C"
+        )
+    );
 
     @Mock
     private MedicationMapper medicationMapper;
@@ -281,6 +314,130 @@ public class MedicationRequestMapperTest {
                 .get();
 
         assertThat(medicationRequest.getAuthoredOnElement().getValue()).isEqualTo(expectedAuthoredOn.getValue());
+    }
+
+    @Test
+    void When_MedicationStatementsWithMultipleSupplyPrescribeBasedOnTheSameRepeatSupplyAuthorise_Expect_NoAdditionalPlanCreated() {
+        var ehrExtract = unmarshallEhrExtract(
+            "ehrExtract_MultipleSupplyPrescribeInFulfilmentOfSingleNonAcuteSupplyAuthorise.xml"
+        );
+        when(
+            medicationRequestPlanMapper.mapToPlanMedicationRequest(
+                eq(ehrExtract),
+                any(RCMRMT030101UKEhrComposition.class),
+                any(RCMRMT030101UKMedicationStatement.class),
+                any(RCMRMT030101UKAuthorise.class),
+                any(String.class)
+            )
+        ).thenReturn(
+            (MedicationRequest) new MedicationRequest()
+                .setIntent(MedicationRequestIntent.PLAN)
+                .setExtension(List.of(REPEAT_PRESCRIPTION_EXTENSION))
+                .setId("000EEA41-289B-4B1C-A5AB-421A666A0D2C")
+
+        );
+        when(
+            medicationRequestOrderMapper.mapToOrderMedicationRequest(
+                eq(ehrExtract),
+                any(RCMRMT030101UKEhrComposition.class),
+                any(RCMRMT030101UKMedicationStatement.class),
+                any(RCMRMT030101UKPrescribe.class),
+                any(String.class)
+            )
+        ).thenReturn(
+            new MedicationRequest()
+                .setIntent(MedicationRequestIntent.ORDER)
+                .addBasedOn(REFERENCE_TO_PLAN)
+        );
+
+        var resources = medicationRequestMapper
+            .mapResources(ehrExtract, (Patient) new Patient().setId(PATIENT_ID), List.of(), PRACTISE_CODE
+            );
+
+        var planMedicationRequests = resources.stream()
+            .filter(MedicationRequest.class::isInstance)
+            .map(MedicationRequest.class::cast)
+            .filter(medicationRequest -> MedicationRequestIntent.PLAN.equals(medicationRequest.getIntent()));
+
+        var orderMedicationRequests = resources.stream()
+            .filter(MedicationRequest.class::isInstance)
+            .map(MedicationRequest.class::cast)
+            .filter(medicationRequest -> MedicationRequestIntent.ORDER.equals(medicationRequest.getIntent()))
+            .toList();
+
+        assertAll(
+            () -> assertThat(planMedicationRequests).as("Plans").hasSize(1),
+            () -> assertThat(orderMedicationRequests).as("Orders").hasSize(2)
+        );
+    }
+
+    @Test
+    void When_DuplicatingAcutePlan_Expect_TheEarliestOrderReferencesTheOriginalPlan() {
+        var ehrExtract = unmarshallEhrExtract(
+            "ehrExtract_MultipleSupplyPrescribeInFulfilmentOfSingleAcuteSupplyAuthorise.xml"
+        );
+
+        final var initialPlanId = "000EEA41-289B-4B1C-A5AB-421A666A0D2C";
+
+        when(
+            medicationRequestPlanMapper.mapToPlanMedicationRequest(
+                eq(ehrExtract),
+                any(RCMRMT030101UKEhrComposition.class),
+                any(RCMRMT030101UKMedicationStatement.class),
+                any(RCMRMT030101UKAuthorise.class),
+                any(String.class)
+            )
+        ).thenReturn(
+            (MedicationRequest) new MedicationRequest()
+                .setIntent(MedicationRequestIntent.PLAN)
+                .setExtension(List.of(ACUTE_PRESCRIPTION_EXTENSION))
+                .setId(initialPlanId)
+        );
+
+        when(
+            medicationRequestOrderMapper.mapToOrderMedicationRequest(
+                eq(ehrExtract),
+                any(RCMRMT030101UKEhrComposition.class),
+                any(RCMRMT030101UKMedicationStatement.class),
+                any(RCMRMT030101UKPrescribe.class),
+                any(String.class)
+            )
+        ).thenReturn(
+            new MedicationRequest()
+                .setIntent(MedicationRequestIntent.ORDER)
+                .addBasedOn(REFERENCE_TO_PLAN)
+                .setDispenseRequest(
+                    new MedicationRequestDispenseRequestComponent()
+                        .setValidityPeriod(
+                            new Period().setStartElement(DateFormatUtil.parseToDateTimeType("20240101"))
+                        )
+                ),
+            new MedicationRequest()
+                .setIntent(MedicationRequestIntent.ORDER)
+                .addBasedOn(REFERENCE_TO_PLAN)
+                .setDispenseRequest(
+                    new MedicationRequestDispenseRequestComponent()
+                        .setValidityPeriod(
+                            new Period().setStartElement(DateFormatUtil.parseToDateTimeType("20240202"))
+                        )
+                )
+        );
+
+        var resources = medicationRequestMapper
+            .mapResources(ehrExtract, (Patient) new Patient().setId(PATIENT_ID), List.of(), PRACTISE_CODE
+            );
+
+        var firstOrderMedicationRequest = resources.stream()
+            .filter(MedicationRequest.class::isInstance)
+            .map(MedicationRequest.class::cast)
+            .filter(medicationRequest -> MedicationRequestIntent.ORDER.equals(medicationRequest.getIntent()))
+            .findFirst()
+            .orElseThrow();
+
+        assertAll(
+            () -> assertThat(firstOrderMedicationRequest.getBasedOn().getFirst().getReference())
+                .isEqualTo("MedicationRequest/"+initialPlanId)
+        );
     }
 
     @Test
