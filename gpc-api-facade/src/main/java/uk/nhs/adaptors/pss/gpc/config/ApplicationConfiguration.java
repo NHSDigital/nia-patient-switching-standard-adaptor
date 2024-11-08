0b6fe31ce2fe87486bb234f17ba386575ce9d1f7
hospel <12736698+ole4ryb@users.noreply.github.com>
2024-11-08 10:47:58 +0000

    changing from bean config to post-processor config as the bean clashed with the one defined in common

--- gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/config/ApplicationConfiguration.java
+++ gpc-api-facade/src/main/java/uk/nhs/adaptors/pss/gpc/config/ApplicationConfiguration.java
@@ -1,28 +1,27 @@
 package uk.nhs.adaptors.pss.gpc.config;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.hl7.fhir.dstu3.model.Parameters;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-
 import uk.nhs.adaptors.common.config.CommonConfiguration;
 import uk.nhs.adaptors.common.util.fhir.FhirParser;
 import uk.nhs.adaptors.connector.config.DbConnectorConfiguration;
 import uk.nhs.adaptors.pss.gpc.config.serialization.ParametersDeserializer;
 
+
 @Configuration
 @Import({DbConnectorConfiguration.class, CommonConfiguration.class})
-public class ApplicationConfiguration {
-    @Bean
-    public ObjectMapper objectMapper(FhirParser fhirParser) {
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(Parameters.class, new ParametersDeserializer(fhirParser));
-        return new ObjectMapper().registerModule(module);
-    }
+public class ApplicationConfiguration implements BeanPostProcessor {
+
+    @Autowired
+    private FhirParser fhirParser;
 
     @Bean
     public MappingJackson2HttpMessageConverter jacksonConverter(ObjectMapper objectMapper) {
@@ -30,5 +29,15 @@ public class ApplicationConfiguration {
         jacksonConverter.setObjectMapper(objectMapper);
 
         return jacksonConverter;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof ObjectMapper objectMapper) {
+            SimpleModule module = new SimpleModule();
+            module.addDeserializer(Parameters.class, new ParametersDeserializer(fhirParser));
+            objectMapper.registerModule(module);
+        }
+        return bean;
     }
 }
